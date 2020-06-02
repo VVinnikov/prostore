bf590ac6e69e4793fd171f7a9892d783a8c6a0d6
d.voronkov <d.voronkov@corp.mail.ru>
2020-06-02 11:07:00 +0300

    DTM-348 fix services injection

--- dtm-query-execution-core/src/main/java/ru/ibs/dtm/query/execution/core/service/impl/QueryDispatcherImpl.java
+++ dtm-query-execution-core/src/main/java/ru/ibs/dtm/query/execution/core/service/impl/QueryDispatcherImpl.java
@@ -14,6 +14,7 @@ import ru.ibs.dtm.query.execution.plugin.api.request.DatamartRequest;
 import ru.ibs.dtm.query.execution.plugin.api.service.DatamartExecutionService;
 import ru.ibs.dtm.query.execution.plugin.api.service.SqlProcessingType;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -23,16 +24,17 @@ public class QueryDispatcherImpl implements QueryDispatcher {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(QueryDispatcherImpl.class);
 
-	private final Map<SqlProcessingType, DatamartExecutionService<RequestContext<? extends DatamartRequest>, AsyncResult<QueryResult>>> serviceMap;
+	private final Map<SqlProcessingType, DatamartExecutionService<RequestContext<? extends DatamartRequest>, AsyncResult<QueryResult>>> serviceMap = new HashMap<>();
 
 	@Autowired
-	public QueryDispatcherImpl(List<DatamartExecutionService<RequestContext<? extends DatamartRequest>, AsyncResult<QueryResult>>> serviceMap) {
-		this.serviceMap = serviceMap.stream()
-				.collect(Collectors.toMap(DatamartExecutionService::getSqlProcessingType, it -> it));
+	public QueryDispatcherImpl(List<DatamartExecutionService<? extends RequestContext<? extends DatamartRequest>, AsyncResult<QueryResult>>> services) {
+		for (DatamartExecutionService<? extends RequestContext<? extends DatamartRequest>, AsyncResult<QueryResult>> es : services) {
+			serviceMap.put(es.getSqlProcessingType(), (DatamartExecutionService<RequestContext<? extends DatamartRequest>, AsyncResult<QueryResult>>) es);
+		}
 	}
 
 	@Override
-	public void dispatch(RequestContext<? extends DatamartRequest> context, Handler<AsyncResult<QueryResult>> asyncResultHandler) {
+	public void dispatch(RequestContext<?> context, Handler<AsyncResult<QueryResult>> asyncResultHandler) {
 		try {
 			serviceMap.get(context.getProcessingType())
 					.execute(context, asyncResultHandler);

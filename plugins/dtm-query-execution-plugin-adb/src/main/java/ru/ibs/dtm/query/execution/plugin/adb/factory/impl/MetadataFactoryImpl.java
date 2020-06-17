a1f42f199bfb8e18f5b442fd350336ee6764fa54
Ivan <lapain.dev@gmail.com>
2020-06-17 15:09:08 +0300

    DTM-380 Implemented a Sharding Key installation in DISTRIBUTED BY block

--- plugins/dtm-query-execution-plugin-adb/src/main/java/ru/ibs/dtm/query/execution/plugin/adb/factory/impl/MetadataFactoryImpl.java
+++ plugins/dtm-query-execution-plugin-adb/src/main/java/ru/ibs/dtm/query/execution/plugin/adb/factory/impl/MetadataFactoryImpl.java
@@ -3,151 +3,39 @@ package ru.ibs.dtm.query.execution.plugin.adb.factory.impl;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import ru.ibs.dtm.common.model.ddl.ClassField;
 import ru.ibs.dtm.common.model.ddl.ClassTable;
-import ru.ibs.dtm.common.model.ddl.ClassTypeUtil;
+import ru.ibs.dtm.query.execution.plugin.adb.factory.MetadataSqlFactory;
 import ru.ibs.dtm.query.execution.plugin.adb.factory.MetadataFactory;
 import ru.ibs.dtm.query.execution.plugin.adb.service.impl.query.AdbQueryExecutor;
 
-import java.util.Collection;
-import java.util.stream.Collectors;
-
-import static java.util.stream.Collectors.toList;
-
-@Service
 @Slf4j
+@Service
+@RequiredArgsConstructor
 public class MetadataFactoryImpl implements MetadataFactory {
 
-  /**
-   * Название таблицы актуальных данных
-   */
-  public final static String ACTUAL_TABLE = "actual";
-  /**
-   * Название таблицы истории
-   */
-  public final static String HISTORY_TABLE = "history";
-  /**
-   * Название стейджинг таблицы
-   */
-  public final static String STAGING_TABLE = "staging";
-  /**
-   * Системное поле номера дельты
-   */
-  public final static String SYS_FROM_ATTR = "sys_from";
-  /**
-   * Системное поле максимального номера дельты
-   */
-  public final static String SYS_TO_ATTR = "sys_to";
-  /**
-   * Системное поле операции над объектом
-   */
-  public final static String SYS_OP_ATTR = "sys_op";
-  /**
-   * Системное поле идентификатора запроса
-   */
-  public final static String REQ_ID_ATTR = "req_id";
+    private final AdbQueryExecutor adbQueryExecutor;
+    private final MetadataSqlFactory sqlFactory;
 
-  private final static String DROP_TABLE = "DROP TABLE IF EXISTS ";
-
-  private AdbQueryExecutor adbQueryExecutor;
-
-  public MetadataFactoryImpl(AdbQueryExecutor adbQueryExecutor) {
-    this.adbQueryExecutor = adbQueryExecutor;
-  }
-
-  @Override
-  public void apply(ClassTable classTable, Handler<AsyncResult<Void>> handler) {
-    String dropSql = dropTableScript(classTable);
-    adbQueryExecutor.executeUpdate(dropSql, ar -> {
-      if (ar.succeeded()) {
-        String createSql = createTableScripts(classTable);
-        adbQueryExecutor.executeUpdate(createSql, handler);
-      } else {
-        log.error("Ошибка исполнения метода apply плагина ADB", ar.cause());
-        handler.handle(Future.failedFuture(ar.cause()));
-      }
-    });
-  }
-
-  @Override
-  public void purge(ClassTable classTable, Handler<AsyncResult<Void>> handler) {
-    String dropSql = dropTableScript(classTable);
-    adbQueryExecutor.executeUpdate(dropSql, handler);
-  }
-
-  private String createTableScripts(ClassTable classTable) {
-    StringBuilder sb = new StringBuilder()
-      .append(createTableScript(classTable, classTable.getNameWithSchema() + "_" + ACTUAL_TABLE, false))
-      .append("; ")
-      .append(createTableScript(classTable, classTable.getNameWithSchema() + "_" + HISTORY_TABLE, false))
-      .append("; ")
-      .append(createTableScript(classTable, classTable.getNameWithSchema() + "_" + STAGING_TABLE, true))
-      .append("; ");
-    return sb.toString();
-  }
-
-  private String dropTableScript(ClassTable classTable) {
-    StringBuilder sb = new StringBuilder()
-      .append(DROP_TABLE).append(classTable.getNameWithSchema()).append("_").append(ACTUAL_TABLE)
-      .append("; ")
-      .append(DROP_TABLE).append(classTable.getNameWithSchema()).append("_").append(HISTORY_TABLE)
-      .append("; ")
-      .append(DROP_TABLE).append(classTable.getNameWithSchema()).append("_").append(STAGING_TABLE)
-      .append("; ");
-    return sb.toString();
-  }
-
-  private String createTableScript(ClassTable classTable, String tableName, boolean addRegId) {
-    StringBuilder sb = new StringBuilder()
-      .append("CREATE TABLE ").append(tableName)
-      .append(" (");
-    final String[] prefix = {" "};
-    classTable.getFields().forEach(it -> {
-      sb.append(prefix[0])
-        .append(it.getName())
-        .append(" ")
-        .append(ClassTypeUtil.pgFromMariaType(it.getType().name().toLowerCase()));
-      if (it.getType().name().equalsIgnoreCase("varchar") && it.getSize() != null) {
-        sb.append("(").append(it.getSize()).append(") ");
-      } else {
-        sb.append(" ");
-      }
-      if (!it.getNull()) {
-        sb.append("NOT NULL");
-      }
-      prefix[0] = ", ";
-    });
-    sb.append(prefix[0])
-      .append(SYS_FROM_ATTR)
-      .append(" ")
-      .append("bigint")
-      .append(prefix[0])
-      .append(SYS_TO_ATTR)
-      .append(" ")
-      .append("bigint")
-      .append(prefix[0])
-      .append(SYS_OP_ATTR)
-      .append(" ")
-      .append("int");
-    if (addRegId) {
-      sb.append(prefix[0])
-        .append(REQ_ID_ATTR)
-        .append(" ")
-        .append("varchar(36)");
+    @Override
+    public void apply(ClassTable classTable, Handler<AsyncResult<Void>> handler) {
+        String dropSql = sqlFactory.createDropTableScript(classTable);
+        adbQueryExecutor.executeUpdate(dropSql, ar -> {
+            if (ar.succeeded()) {
+                String createSql = sqlFactory.createTableScripts(classTable);
+                adbQueryExecutor.executeUpdate(createSql, handler);
+            } else {
+                log.error("Ошибка исполнения метода apply плагина ADB", ar.cause());
+                handler.handle(Future.failedFuture(ar.cause()));
+            }
+        });
     }
-    Collection<ClassField> pkList = classTable.getFields().stream().filter(f -> f.getPrimaryOrder() != null).collect(toList());
-    if (pkList.size() > 0) {
-      sb.append(prefix[0])
-              .append("constraint ")
-              .append("pk_")
-              .append(tableName.replace('.', '_'))
-              .append(" primary key (")
-              .append(pkList.stream().map(ClassField::getName).collect(Collectors.joining(", ")))
-              .append(")");
+
+    @Override
+    public void purge(ClassTable classTable, Handler<AsyncResult<Void>> handler) {
+        String dropSql = sqlFactory.createDropTableScript(classTable);
+        adbQueryExecutor.executeUpdate(dropSql, handler);
     }
-    sb.append(")");
-    return sb.toString();
-  }
 }
---
+++ plugins/dtm-query-execution-plugin-adb/src/main/java/ru/ibs/dtm/query/execution/plugin/adb/factory/impl/MetadataSqlFactoryImpl.java
@@ -0,0 +1,170 @@
+package ru.ibs.dtm.query.execution.plugin.adb.factory.impl;
+
+import lombok.val;
+import org.springframework.stereotype.Component;
+import ru.ibs.dtm.common.model.ddl.ClassField;
+import ru.ibs.dtm.common.model.ddl.ClassTable;
+import ru.ibs.dtm.common.model.ddl.ClassTypeUtil;
+import ru.ibs.dtm.query.execution.plugin.adb.factory.MetadataSqlFactory;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
+@Component
+public class MetadataSqlFactoryImpl implements MetadataSqlFactory {
+    /**
+     * Name of the table of actual data
+     */
+    public final static String ACTUAL_TABLE = "actual";
+    /**
+     * History table name
+     */
+    public final static String HISTORY_TABLE = "history";
+    /**
+     * Name staging table
+     */
+    public final static String STAGING_TABLE = "staging";
+    /**
+     * Delta Number System Field
+     */
+    public final static String SYS_FROM_ATTR = "sys_from";
+    /**
+     * System field of maximum delta number
+     */
+    public final static String SYS_TO_ATTR = "sys_to";
+    /**
+     * System field of operation on an object
+     */
+    public final static String SYS_OP_ATTR = "sys_op";
+    /**
+     * Request ID system field
+     */
+    public final static String REQ_ID_ATTR = "req_id";
+    public static final String DELIMITER = ", ";
+    private final static String DROP_TABLE = "DROP TABLE IF EXISTS ";
+
+    @Override
+    public String createDropTableScript(ClassTable classTable) {
+        return new StringBuilder()
+                .append(DROP_TABLE).append(classTable.getNameWithSchema()).append("_").append(ACTUAL_TABLE)
+                .append("; ")
+                .append(DROP_TABLE).append(classTable.getNameWithSchema()).append("_").append(HISTORY_TABLE)
+                .append("; ")
+                .append(DROP_TABLE).append(classTable.getNameWithSchema()).append("_").append(STAGING_TABLE)
+                .append("; ")
+                .toString();
+    }
+
+    @Override
+    public String createTableScripts(ClassTable classTable) {
+        return new StringBuilder()
+                .append(createTableScript(classTable, classTable.getNameWithSchema() + "_" + ACTUAL_TABLE, false))
+                .append("; ")
+                .append(createTableScript(classTable, classTable.getNameWithSchema() + "_" + HISTORY_TABLE, false))
+                .append("; ")
+                .append(createTableScript(classTable, classTable.getNameWithSchema() + "_" + STAGING_TABLE, true))
+                .append("; ")
+                .toString();
+    }
+
+    private String createTableScript(ClassTable classTable, String tableName, boolean addReqId) {
+        val initDelimiter = classTable.getFields().isEmpty() ? " " : DELIMITER;
+        val sb = new StringBuilder()
+                .append("CREATE TABLE ").append(tableName)
+                .append(" (");
+        appendClassTableFields(sb, classTable.getFields());
+        appendSystemColumns(sb, initDelimiter);
+        if (addReqId) {
+            appendReqIdColumn(sb);
+        }
+        val pkList = getPrimaryKeyList(classTable.getFields());
+        if (pkList.size() > 0) {
+            appendPrimaryKeys(sb, tableName, pkList);
+        }
+        sb.append(")");
+        val shardingKeyList = getShardingKeyList(classTable.getFields());
+        if (shardingKeyList.size() > 0) {
+            appendShardingKeys(sb, shardingKeyList);
+        }
+        return sb.toString();
+    }
+
+    private List<ClassField> getPrimaryKeyList(List<ClassField> fields) {
+        return fields.stream()
+                .filter(f -> f.getPrimaryOrder() != null)
+                .sorted(Comparator.comparing(ClassField::getPrimaryOrder))
+                .collect(toList());
+    }
+
+    private List<ClassField> getShardingKeyList(List<ClassField> fields) {
+        return fields.stream()
+                .filter(f -> f.getShardingOrder() != null)
+                .sorted(Comparator.comparing(ClassField::getShardingOrder))
+                .collect(toList());
+    }
+
+    private void appendClassTableFields(StringBuilder builder, List<ClassField> fields) {
+        val columns = fields.stream()
+                .map(this::getColumnDDLByField)
+                .collect(Collectors.joining(DELIMITER));
+        builder.append(columns);
+    }
+
+    private String getColumnDDLByField(ClassField field) {
+        val sb = new StringBuilder();
+        sb.append(field.getName())
+                .append(" ")
+                .append(ClassTypeUtil.pgFromMariaType(field.getType().name().toLowerCase()));
+        if (field.getType().name().equalsIgnoreCase("varchar") && field.getSize() != null) {
+            sb.append("(").append(field.getSize()).append(") ");
+        } else {
+            sb.append(" ");
+        }
+        if (! field.getNull()) {
+            sb.append("NOT NULL");
+        }
+        return sb.toString();
+    }
+
+    private void appendPrimaryKeys(StringBuilder builder, String tableName, Collection<ClassField> pkList) {
+        builder.append(DELIMITER)
+                .append("constraint ")
+                .append("pk_")
+                .append(tableName.replace('.', '_'))
+                .append(" primary key (")
+                .append(pkList.stream().map(ClassField::getName).collect(Collectors.joining(DELIMITER)))
+                .append(")");
+    }
+
+    private void appendShardingKeys(StringBuilder builder, Collection<ClassField> skList) {
+        builder.append(" DISTRIBUTED BY (")
+                .append(skList.stream().map(ClassField::getName).collect(Collectors.joining(DELIMITER)))
+                .append(")");
+    }
+
+    private void appendReqIdColumn(StringBuilder builder) {
+        builder.append(DELIMITER)
+                .append(REQ_ID_ATTR)
+                .append(" ")
+                .append("varchar(36)");
+    }
+
+    private void appendSystemColumns(StringBuilder builder, String delimiter) {
+        builder.append(delimiter)
+                .append(SYS_FROM_ATTR)
+                .append(" ")
+                .append("bigint")
+                .append(DELIMITER)
+                .append(SYS_TO_ATTR)
+                .append(" ")
+                .append("bigint")
+                .append(DELIMITER)
+                .append(SYS_OP_ATTR)
+                .append(" ")
+                .append("int");
+    }
+}

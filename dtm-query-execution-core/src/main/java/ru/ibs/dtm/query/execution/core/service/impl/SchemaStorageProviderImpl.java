3b08623b6f6d38a01e10a808c0c285e02318bedb
Ivan <lapain.dev@gmail.com>
2020-06-18 08:33:25 +0300

    DTM-383 format

--- dtm-query-execution-core/src/main/java/ru/ibs/dtm/query/execution/core/service/impl/SchemaStorageProviderImpl.java
+++ dtm-query-execution-core/src/main/java/ru/ibs/dtm/query/execution/core/service/impl/SchemaStorageProviderImpl.java
@@ -7,9 +7,6 @@ import io.vertx.core.Handler;
 import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.ibs.dtm.query.execution.core.dao.ServiceDao;
 import ru.ibs.dtm.query.execution.core.dto.DatamartEntity;
@@ -23,112 +20,109 @@ import java.util.UUID;
 @Service
 @Slf4j
 public class SchemaStorageProviderImpl implements SchemaStorageProvider {
+    private final ServiceDao serviceDao;
 
-  private final ServiceDao serviceDao;
+    public SchemaStorageProviderImpl(ServiceDao serviceDao) {
+        this.serviceDao = serviceDao;
+    }
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(SchemaStorageProviderImpl.class);
-
-  public SchemaStorageProviderImpl(ServiceDao serviceDao) {
-    this.serviceDao = serviceDao;
-  }
-
-  @Override
-  public void getLogicalSchema(String datamartMnemonic, Handler<AsyncResult<JsonObject>> asyncResultHandler) {
-    serviceDao.getEntitiesMeta(datamartMnemonic, ar -> {
-      if (ar.succeeded()) {
-        List<DatamartEntity> dmEntity = ar.result();
-        List<DatamartClass> dmClassResult = new ArrayList<>();
-        fillDmArray(dmEntity.size() - 1, dmEntity, dmClassResult, ar2 -> {
-          if (ar2.succeeded()) {
-            Datamart datamart = new Datamart();
-            datamart.setId(UUID.randomUUID());
-            datamart.setMnemonic(datamartMnemonic);
-            datamart.setDatamartClassess(ar2.result());
-            JsonObject jsonResult = new JsonObject(Json.encode(datamart));
-            LOGGER.info("Метаданные для запроса получены. Схема: {}",datamartMnemonic);
-            LOGGER.trace("Метаданные: [{}]",jsonResult.toString());
-            asyncResultHandler.handle(Future.succeededFuture(jsonResult));
-          } else {
-            asyncResultHandler.handle(Future.failedFuture(ar2.cause()));
-          }
+    @Override
+    public void getLogicalSchema(String datamartMnemonic, Handler<AsyncResult<JsonObject>> asyncResultHandler) {
+        serviceDao.getEntitiesMeta(datamartMnemonic, ar -> {
+            if (ar.succeeded()) {
+                List<DatamartEntity> dmEntity = ar.result();
+                List<DatamartClass> dmClassResult = new ArrayList<>();
+                fillDmArray(dmEntity.size() - 1, dmEntity, dmClassResult, ar2 -> {
+                    if (ar2.succeeded()) {
+                        Datamart datamart = new Datamart();
+                        datamart.setId(UUID.randomUUID());
+                        datamart.setMnemonic(datamartMnemonic);
+                        datamart.setDatamartClassess(ar2.result());
+                        JsonObject jsonResult = new JsonObject(Json.encode(datamart));
+                        log.info("Метаданные для запроса получены. Схема: {}", datamartMnemonic);
+                        log.trace("Метаданные: [{}]", jsonResult.toString());
+                        asyncResultHandler.handle(Future.succeededFuture(jsonResult));
+                    } else {
+                        asyncResultHandler.handle(Future.failedFuture(ar2.cause()));
+                    }
+                });
+            } else {
+                asyncResultHandler.handle(Future.failedFuture(ar.cause()));
+            }
         });
-      } else {
-        asyncResultHandler.handle(Future.failedFuture(ar.cause()));
-      }
-    });
-  }
+    }
 
-  private void fillDmArray(int currentIteration, List<DatamartEntity> dmEntities, List<DatamartClass> dmClassResult, Handler<AsyncResult<List<DatamartClass>>> asyncResult) {
-    if (currentIteration < 0) {
-      asyncResult.handle(Future.succeededFuture(dmClassResult));
-    } else {
-      DatamartEntity datamartEntity = dmEntities.get(currentIteration);
-      serviceDao.getAttributesMeta(datamartEntity.getDatamartMnemonic(), datamartEntity.getMnemonic(), ar -> {
-        if (ar.succeeded()) {
-          List<ClassAttribute> classAttributes = new ArrayList<>();
-          DatamartClass dmClass = new DatamartClass();
-          dmClass.setId(UUID.randomUUID());
-          dmClass.setMnemonic(datamartEntity.getMnemonic());
-          dmClass.setLabel(datamartEntity.getMnemonic());
-          ar.result().forEach(attr -> {
-            ClassAttribute classAttribute = new ClassAttribute();
-            classAttribute.setId(UUID.randomUUID());
-            classAttribute.setMnemonic(attr.getMnemonic());
-            classAttribute.setType(mapColumnType(attr.getDataType()));
-            classAttributes.add(classAttribute);
-          });
-          dmClass.setClassAttributes(classAttributes);
-          dmClassResult.add(dmClass);
-          fillDmArray(currentIteration - 1, dmEntities, dmClassResult, asyncResult);
+    private void fillDmArray(int currentIteration, List<DatamartEntity> dmEntities, List<DatamartClass> dmClassResult, Handler<AsyncResult<List<DatamartClass>>> asyncResult) {
+        if (currentIteration < 0) {
+            asyncResult.handle(Future.succeededFuture(dmClassResult));
         } else {
-          asyncResult.handle(Future.failedFuture(ar.cause()));
+            DatamartEntity datamartEntity = dmEntities.get(currentIteration);
+            serviceDao.getAttributesMeta(datamartEntity.getDatamartMnemonic(), datamartEntity.getMnemonic(), ar -> {
+                if (ar.succeeded()) {
+                    List<ClassAttribute> classAttributes = new ArrayList<>();
+                    DatamartClass dmClass = new DatamartClass();
+                    dmClass.setId(UUID.randomUUID());
+                    dmClass.setMnemonic(datamartEntity.getMnemonic());
+                    dmClass.setLabel(datamartEntity.getMnemonic());
+                    ar.result().forEach(attr -> {
+                        ClassAttribute classAttribute = new ClassAttribute();
+                        classAttribute.setId(UUID.randomUUID());
+                        classAttribute.setMnemonic(attr.getMnemonic());
+                        classAttribute.setType(mapColumnType(attr.getDataType()));
+                        classAttributes.add(classAttribute);
+                    });
+                    dmClass.setClassAttributes(classAttributes);
+                    dmClassResult.add(dmClass);
+                    fillDmArray(currentIteration - 1, dmEntities, dmClassResult, asyncResult);
+                } else {
+                    asyncResult.handle(Future.failedFuture(ar.cause()));
+                }
+            });
         }
-      });
     }
-  }
 
-  private TypeMessage mapColumnType(String dataType) {
-    TypeMessage typeMessage = new TypeMessage();
-    typeMessage.setId(UUID.randomUUID());
-    ColumnType type = null;
-    switch (dataType.toLowerCase()) {
-      case "varchar":
-      case "char":
-        type = ColumnType.STRING;
-        break;
-      case "bigint":
-        type = ColumnType.LONG;
-        break;
-      case "int":
-      case "integer":
-      case "tinyint":
-        type = ColumnType.INTEGER;
-        break;
-      case "date":
-        type = ColumnType.DATE;
-        break;
-      case "datetime":
-      case "timestamp":
-        type = ColumnType.TIMESTAMP;
-        break;
-      case "decimal":
-      case "numeric":
-        type = ColumnType.BIG_DECIMAL;
-        break;
-      case "float":
-        type = ColumnType.FLOAT;
-        break;
-      case "double":
-        type = ColumnType.DOUBLE;
-        break;
-      case "boolean":
-        type = ColumnType.BOOLEAN;
-        break;
-      default:
-        type = ColumnType.ANY;
-        break;
+    private TypeMessage mapColumnType(String dataType) {
+        TypeMessage typeMessage = new TypeMessage();
+        typeMessage.setId(UUID.randomUUID());
+        ColumnType type = null;
+        switch (dataType.toLowerCase()) {
+            case "varchar":
+            case "char":
+                type = ColumnType.STRING;
+                break;
+            case "bigint":
+                type = ColumnType.LONG;
+                break;
+            case "int":
+            case "integer":
+            case "tinyint":
+                type = ColumnType.INTEGER;
+                break;
+            case "date":
+                type = ColumnType.DATE;
+                break;
+            case "datetime":
+            case "timestamp":
+                type = ColumnType.TIMESTAMP;
+                break;
+            case "decimal":
+            case "numeric":
+                type = ColumnType.BIG_DECIMAL;
+                break;
+            case "float":
+                type = ColumnType.FLOAT;
+                break;
+            case "double":
+                type = ColumnType.DOUBLE;
+                break;
+            case "boolean":
+                type = ColumnType.BOOLEAN;
+                break;
+            default:
+                type = ColumnType.ANY;
+                break;
+        }
+        typeMessage.setValue(type);
+        return typeMessage;
     }
-    typeMessage.setValue(type);
-    return typeMessage;
-  }
 }
--- dtm-query-execution-core/src/main/java/ru/ibs/dtm/query/execution/core/service/impl/TargetDatabaseDefinitionServiceImpl.java
+++ dtm-query-execution-core/src/main/java/ru/ibs/dtm/query/execution/core/service/impl/TargetDatabaseDefinitionServiceImpl.java
@@ -25,78 +25,78 @@ import java.util.List;
 @Service
 public class TargetDatabaseDefinitionServiceImpl implements TargetDatabaseDefinitionService {
 
-  private final DataSourcePluginService pluginService;
-  private final HintExtractor hintExtractor;
+    private final DataSourcePluginService pluginService;
+    private final HintExtractor hintExtractor;
 
-  @Autowired
+    @Autowired
 
-  public TargetDatabaseDefinitionServiceImpl(DataSourcePluginService pluginService,
-                                             HintExtractor hintExtractor) {
-    this.pluginService = pluginService;
-    this.hintExtractor = hintExtractor;
-  }
-
-  @Override
-  public void getTargetSource(QueryRequest request, Handler<AsyncResult<QuerySourceRequest>> handler) {
-    hintExtractor.extractHint(request, ar -> {
-      if (ar.succeeded()) {
-        QuerySourceRequest querySourceRequest = ar.result();
-        if (querySourceRequest.getSourceType() != null) {
-          handler.handle(Future.succeededFuture(querySourceRequest));
-        } else {
-          getTargetSourceWithoutHint(request, handler);
-        }
-      } else {
-        handler.handle(Future.failedFuture(ar.cause()));
-      }
-    });
-  }
-
-  private void getTargetSourceWithoutHint(QueryRequest request, Handler<AsyncResult<QuerySourceRequest>> handler) {
-    if (CollectionUtils.isEmpty(MetaDataQueryPreparer.findInformationSchemaViews(request.getSql()))) {
-      getTargetSourceFromCost(request, ar -> {
-        if (ar.succeeded()) {
-          handler.handle(Future.succeededFuture(
-            new QuerySourceRequest(
-              request.copy(),
-              ar.result())));
-        } else {
-          handler.handle(Future.failedFuture(ar.cause()));
-        }
-      });
-    } else {
-      handler.handle(Future.succeededFuture(
-        new QuerySourceRequest(
-          request.copy(),
-          SourceType.INFORMATION_SCHEMA)));
+    public TargetDatabaseDefinitionServiceImpl(DataSourcePluginService pluginService,
+                                               HintExtractor hintExtractor) {
+        this.pluginService = pluginService;
+        this.hintExtractor = hintExtractor;
     }
-  }
 
-  private void getTargetSourceFromCost(QueryRequest request, Handler<AsyncResult<SourceType>> handler) {
-    List<Future> sourceTypeCost = new ArrayList<>();
-    pluginService.getSourceTypes().forEach(sourceType -> {
-      sourceTypeCost.add(Future.future(p ->
-        pluginService.calcQueryCost(sourceType, new QueryCostRequestContext(new QueryCostRequest(request)), ar -> {
-          if (ar.succeeded()) {
-            p.complete(Pair.of(sourceType, ar.result()));
-          } else {
-            p.fail(ar.cause());
-          }
-        }))
-      );
-    });
-    CompositeFuture.all(sourceTypeCost).onComplete(
-      ar -> {
-        if (ar.succeeded()) {
-          SourceType sourceType = ar.result().list().stream()
-            .map(res -> (Pair<SourceType, Integer>) res)
-            .min(Comparator.comparingInt(Pair::getValue))
-            .map(Pair::getKey)
-            .orElse(null);
-          handler.handle(Future.succeededFuture(sourceType));
+    @Override
+    public void getTargetSource(QueryRequest request, Handler<AsyncResult<QuerySourceRequest>> handler) {
+        hintExtractor.extractHint(request, ar -> {
+            if (ar.succeeded()) {
+                QuerySourceRequest querySourceRequest = ar.result();
+                if (querySourceRequest.getSourceType() != null) {
+                    handler.handle(Future.succeededFuture(querySourceRequest));
+                } else {
+                    getTargetSourceWithoutHint(request, handler);
+                }
+            } else {
+                handler.handle(Future.failedFuture(ar.cause()));
+            }
+        });
+    }
+
+    private void getTargetSourceWithoutHint(QueryRequest request, Handler<AsyncResult<QuerySourceRequest>> handler) {
+        if (CollectionUtils.isEmpty(MetaDataQueryPreparer.findInformationSchemaViews(request.getSql()))) {
+            getTargetSourceFromCost(request, ar -> {
+                if (ar.succeeded()) {
+                    handler.handle(Future.succeededFuture(
+                            new QuerySourceRequest(
+                                    request.copy(),
+                                    ar.result())));
+                } else {
+                    handler.handle(Future.failedFuture(ar.cause()));
+                }
+            });
         } else {
-          handler.handle(Future.failedFuture(ar.cause()));
+            handler.handle(Future.succeededFuture(
+                    new QuerySourceRequest(
+                            request.copy(),
+                            SourceType.INFORMATION_SCHEMA)));
         }
-      });
-  }
+    }
+
+    private void getTargetSourceFromCost(QueryRequest request, Handler<AsyncResult<SourceType>> handler) {
+        List<Future> sourceTypeCost = new ArrayList<>();
+        pluginService.getSourceTypes().forEach(sourceType -> {
+            sourceTypeCost.add(Future.future(p ->
+                    pluginService.calcQueryCost(sourceType, new QueryCostRequestContext(new QueryCostRequest(request)), ar -> {
+                        if (ar.succeeded()) {
+                            p.complete(Pair.of(sourceType, ar.result()));
+                        } else {
+                            p.fail(ar.cause());
+                        }
+                    }))
+            );
+        });
+        CompositeFuture.all(sourceTypeCost).onComplete(
+                ar -> {
+                    if (ar.succeeded()) {
+                        SourceType sourceType = ar.result().list().stream()
+                                .map(res -> (Pair<SourceType, Integer>) res)
+                                .min(Comparator.comparingInt(Pair::getValue))
+                                .map(Pair::getKey)
+                                .orElse(null);
+                        handler.handle(Future.succeededFuture(sourceType));
+                    } else {
+                        handler.handle(Future.failedFuture(ar.cause()));
+                    }
+                });
+    }
 }

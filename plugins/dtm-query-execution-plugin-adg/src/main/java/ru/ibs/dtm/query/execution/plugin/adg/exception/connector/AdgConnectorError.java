dd039a059edb68ed37b82911a7c4e6263a48351d
Ivan <lapain.dev@gmail.com>
2020-06-30 15:36:42 +0300

    DTM-365 refactoring

--- plugins/dtm-query-execution-plugin-adg/src/main/java/ru/ibs/dtm/query/execution/plugin/adg/exception/connector/AdgConnectorError.java
+++ plugins/dtm-query-execution-plugin-adg/src/main/java/ru/ibs/dtm/query/execution/plugin/adg/exception/connector/AdgConnectorError.java
@@ -2,10 +2,17 @@ package ru.ibs.dtm.query.execution.plugin.adg.exception.connector;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
 public class AdgConnectorError extends RuntimeException {
     private String code;
     private String message;
+
+    @Override
+    public String getMessage() {
+        return String.format("Code: [%s], message: [%s]", code, message);
+    }
 }
---
+++ plugins/dtm-query-execution-plugin-adg/src/test/java/ru/ibs/dtm/query/execution/plugin/adg/service/impl/mppw/AdgMppwKafkaServiceTest.java
@@ -0,0 +1,13 @@
+package ru.ibs.dtm.query.execution.plugin.adg.service.impl.mppw;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class AdgMppwKafkaServiceTest {
+
+    @Test
+    public void test() {
+    }
+
+}

292a45381c3fd9891404923c18065480c70fd42d
Aleksandr Kapustin <aleksandr.kapustin@corp.mail.ru>
2020-07-08 14:35:51 +0300

    DTM-418. Pre-task. Move from custom model to common

--- plugins/dtm-query-execution-plugin-adqm/src/main/java/ru/ibs/dtm/query/execution/plugin/adqm/calcite/CalciteContextProvider.java
+++ plugins/dtm-query-execution-plugin-adqm/src/main/java/ru/ibs/dtm/query/execution/plugin/adqm/calcite/CalciteContextProvider.java
@@ -14,7 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import ru.ibs.dtm.common.calcite.CalciteContext;
-import ru.ibs.dtm.query.execution.plugin.adqm.model.metadata.Datamart;
+import ru.ibs.dtm.query.execution.model.metadata.Datamart;
 
 import java.util.ArrayList;
 import java.util.List;
--- plugins/dtm-query-execution-plugin-adqm/src/main/java/ru/ibs/dtm/query/execution/plugin/adqm/calcite/schema/CustomTable.java
+++ plugins/dtm-query-execution-plugin-adqm/src/main/java/ru/ibs/dtm/query/execution/plugin/adqm/calcite/schema/CustomTable.java
@@ -10,8 +10,8 @@ import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
 import org.apache.calcite.schema.SchemaPlus;
 import org.apache.calcite.schema.TranslatableTable;
+import ru.ibs.dtm.query.execution.model.metadata.DatamartClass;
 import ru.ibs.dtm.query.execution.plugin.adqm.calcite.CalciteUtil;
-import ru.ibs.dtm.query.execution.plugin.adqm.model.metadata.DatamartClass;
 
 import java.util.ArrayList;
 

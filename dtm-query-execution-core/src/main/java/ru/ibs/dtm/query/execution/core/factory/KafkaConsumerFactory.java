e57017ca69fc90f226eec9f223829f0f31f69de3
Aleksandr Kapustin <aleksandr.kapustin@corp.mail.ru>
2020-07-23 09:29:21 +0300

    DTM-454. Remove redundant KafkaConsumerFactory/KafkaProducerFactory from all modules except kafka-core

--- dtm-query-execution-core/src/main/java/ru/ibs/dtm/query/execution/core/factory/KafkaConsumerFactory.java
+++
@@ -1,9 +0,0 @@
-package ru.ibs.dtm.query.execution.core.factory;
-
-import io.vertx.kafka.client.consumer.KafkaConsumer;
-
-import java.util.Map;
-
-public interface KafkaConsumerFactory<T, S> {
-  KafkaConsumer<T, S> create(Map<String, String> config);
-}

c1ccf3b748f2168b7d7b689eeb25691975de65e4
Ivan Lapa <lin@arenadata.io>
2020-07-23 22:51:06 +0300

    DTM-450 refactoring

--- dtm-common/src/main/java/ru/ibs/dtm/async/AsyncUtils.java
+++ dtm-common/src/main/java/ru/ibs/dtm/async/AsyncUtils.java
@@ -1,1 +1,28 @@
-package ru.ibs.dtm.async;import io.vertx.core.AsyncResult;import io.vertx.core.Future;import io.vertx.core.Handler;import lombok.extern.slf4j.Slf4j;import java.util.function.Consumer;@Slf4jpublic class AsyncUtils {    public static <T, R> Handler<AsyncResult<T>> succeed(Handler<AsyncResult<R>> handler, Consumer<T> succeed) {        return ar -> {            if (ar.succeeded()) {                try {                    succeed.accept(ar.result());                } catch (Exception ex) {                    log.error("Error: ", ex);                    handler.handle(Future.failedFuture(ex));                }            } else {                log.error("Error: ", ar.cause());                handler.handle(Future.failedFuture(ar.cause()));            }        };    }}
+package ru.ibs.dtm.async;
+
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.function.Consumer;
+
+@Slf4j
+public class AsyncUtils {
+    public static <T, R> Handler<AsyncResult<T>> succeed(Handler<AsyncResult<R>> handler, Consumer<T> succeed) {
+        return ar -> {
+            if (ar.succeeded()) {
+                try {
+                    succeed.accept(ar.result());
+                } catch (Exception ex) {
+                    log.error("Error: ", ex);
+                    handler.handle(Future.failedFuture(ex));
+                }
+            } else {
+                log.error("Error: ", ar.cause());
+                handler.handle(Future.failedFuture(ar.cause()));
+            }
+        };
+    }
+}

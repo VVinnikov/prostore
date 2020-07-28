77df460c6b0fcfecce83526abd67f37aa218bd39
Ivan Lapa <lin@arenadata.io>
2020-07-28 18:09:37 +0300

    DTM-468 changed "new line" separators

--- dtm-query-execution-core/src/main/java/ru/ibs/dtm/query/execution/core/configuration/views/SystemViewsConfiguration.java
+++ dtm-query-execution-core/src/main/java/ru/ibs/dtm/query/execution/core/configuration/views/SystemViewsConfiguration.java
@@ -1,1 +1,29 @@
-package ru.ibs.dtm.query.execution.core.configuration.views;import io.vertx.core.Promise;import io.vertx.core.Vertx;import lombok.extern.slf4j.Slf4j;import org.springframework.beans.factory.annotation.Qualifier;import org.springframework.context.annotation.Configuration;import ru.ibs.dtm.query.execution.core.service.schema.SystemDatamartViewsProvider;@Slf4j@Configurationpublic class SystemViewsConfiguration {    public SystemViewsConfiguration(@Qualifier("coreVertx") Vertx vertx, SystemDatamartViewsProvider systemDatamartViewsProvider) {        fetchSystemViews(vertx, systemDatamartViewsProvider);    }    private void fetchSystemViews(Vertx vertx, SystemDatamartViewsProvider systemDatamartViewsProvider) {        vertx.executeBlocking((Promise<Void> in) -> {            systemDatamartViewsProvider.fetchSystemViews().onComplete(in);        }, out -> {            if (out.succeeded()) {                log.debug("System views fetch completed");            } else {                log.debug("System views fetch error", out.cause());            }        });    }}
+package ru.ibs.dtm.query.execution.core.configuration.views;
+
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import ru.ibs.dtm.query.execution.core.service.schema.SystemDatamartViewsProvider;
+
+@Slf4j
+@Configuration
+public class SystemViewsConfiguration {
+
+    public SystemViewsConfiguration(@Qualifier("coreVertx") Vertx vertx, SystemDatamartViewsProvider systemDatamartViewsProvider) {
+        fetchSystemViews(vertx, systemDatamartViewsProvider);
+    }
+
+    private void fetchSystemViews(Vertx vertx, SystemDatamartViewsProvider systemDatamartViewsProvider) {
+        vertx.executeBlocking((Promise<Void> in) -> {
+            systemDatamartViewsProvider.fetchSystemViews().onComplete(in);
+        }, out -> {
+            if (out.succeeded()) {
+                log.debug("System views fetch completed");
+            } else {
+                log.debug("System views fetch error", out.cause());
+            }
+        });
+    }
+}

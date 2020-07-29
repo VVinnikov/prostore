fc1349092ef0944fea5e866afd872f6c90b5a52c
Ivan Lapa <lin@arenadata.io>
2020-07-29 17:20:17 +0300

    DTM-473 fix mapping types for adg

--- plugins/dtm-query-execution-plugin-adg/src/main/java/ru/ibs/dtm/query/execution/plugin/adg/model/cartridge/schema/SpaceAttributeTypeUtil.java
+++ plugins/dtm-query-execution-plugin-adg/src/main/java/ru/ibs/dtm/query/execution/plugin/adg/model/cartridge/schema/SpaceAttributeTypeUtil.java
@@ -7,23 +7,26 @@ import ru.ibs.dtm.common.model.ddl.ColumnType;
  */
 public class SpaceAttributeTypeUtil {
 
-  public static SpaceAttributeTypes toAttributeType(ColumnType type) {
-    switch (type) {
-      case TIMESTAMP:
-      case DATE:
-      case CHAR:
-      case VARCHAR:
-      case ANY:
-        return SpaceAttributeTypes.STRING;
-      case INT:
-      case FLOAT:
-      case DOUBLE:
-      case BIGINT:
-        return SpaceAttributeTypes.NUMBER;
-      case BOOLEAN:
-        return SpaceAttributeTypes.BOOLEAN;
-      default:
-        throw new UnsupportedOperationException(String.format("Не поддержан тип: %s", type));
+    public static SpaceAttributeTypes toAttributeType(ColumnType type) {
+        switch (type) {
+            case UUID:
+            case CHAR:
+            case VARCHAR:
+            case ANY:
+                return SpaceAttributeTypes.STRING;
+            case DATE:
+            case TIME:
+            case TIMESTAMP:
+            case INT:
+            case BIGINT:
+                return SpaceAttributeTypes.INTEGER;
+            case FLOAT:
+            case DOUBLE:
+                return SpaceAttributeTypes.DOUBLE;
+            case BOOLEAN:
+                return SpaceAttributeTypes.BOOLEAN;
+            default:
+                throw new UnsupportedOperationException(String.format("Не поддержан тип: %s", type));
+        }
     }
-  }
 }

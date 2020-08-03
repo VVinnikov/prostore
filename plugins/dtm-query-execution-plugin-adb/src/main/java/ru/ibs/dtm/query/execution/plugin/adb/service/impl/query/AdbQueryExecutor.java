748ef084595b55c24a04b7f585e627efa0c99ada
Ivan Lapa <lin@arenadata.io>
2020-08-03 11:54:17 +0300

    DTM-484 Fixed jdbc driver. Improved sorting of results from ADB

--- plugins/dtm-query-execution-plugin-adb/src/main/java/ru/ibs/dtm/query/execution/plugin/adb/service/impl/query/AdbQueryExecutor.java
+++ plugins/dtm-query-execution-plugin-adb/src/main/java/ru/ibs/dtm/query/execution/plugin/adb/service/impl/query/AdbQueryExecutor.java
@@ -8,103 +8,111 @@ import io.reactiverse.pgclient.impl.ArrayTuple;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
+import io.vertx.core.json.JsonObject;
+import lombok.val;
+import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.ibs.dtm.query.execution.plugin.adb.service.DatabaseExecutor;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class AdbQueryExecutor implements DatabaseExecutor {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(AdbQueryExecutor.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(AdbQueryExecutor.class);
 
-  private final PgPool pool;
-  private final int fetchSize;
+    private final PgPool pool;
+    private final int fetchSize;
 
-  public AdbQueryExecutor(PgPool pool, int fetchSize) {
-    this.pool = pool;
-    this.fetchSize = fetchSize;
-  }
+    public AdbQueryExecutor(PgPool pool, int fetchSize) {
+        this.pool = pool;
+        this.fetchSize = fetchSize;
+    }
 
-  @Override
-  public void execute(String sql, Handler<AsyncResult<List<Map<String, Object>>>> resultHandler) {
-    pool.getConnection(ar1 -> {
-      if (ar1.succeeded()) {
-        PgConnection conn = ar1.result();
-        PgTransaction tx = conn.begin();
-        tx.prepare(sql, ar2 -> {
-          if (ar2.succeeded()) {
-            PgCursor cursor = ar2.result().cursor();
-            do {
-              cursor.read(fetchSize, res -> {
-                if (res.succeeded()) {
-                  List<Map<String, Object>> dataSet = new ArrayList<>();
-                  for (io.reactiverse.pgclient.Row row : res.result()) {
-                    Map<String, Object> values = new HashMap<>();
-                    for (int x = 0; x < row.size(); x++) {
-                      values.put(row.getColumnName(x), row.getValue(x));
+    @Override
+    public void execute(String sql, Handler<AsyncResult<List<JsonObject>>> resultHandler) {
+        pool.getConnection(ar1 -> {
+            if (ar1.succeeded()) {
+                PgConnection conn = ar1.result();
+                PgTransaction tx = conn.begin();
+                tx.prepare(sql, ar2 -> {
+                    if (ar2.succeeded()) {
+                        PgCursor cursor = ar2.result().cursor();
+                        List<Pair<Integer, String>> columnIndexes = new ArrayList<>();
+                        do {
+                            cursor.read(fetchSize, res -> {
+                                if (res.succeeded()) {
+                                    List<JsonObject> dataSet = new ArrayList<>();
+                                    List<String> columnsNames = res.result().columnsNames();
+                                    for (io.reactiverse.pgclient.Row row : res.result()) {
+                                        if (columnIndexes.isEmpty()) {
+                                            for (int x = 0; x < row.size(); x++) {
+                                                val columnName = row.getColumnName(x);
+                                                val index = columnsNames.indexOf(columnName);
+                                                columnIndexes.add(Pair.of(index, columnName));
+                                            }
+                                        }
+                                        JsonObject values = new JsonObject();
+                                        columnIndexes.forEach(p -> values.put(p.getValue(), row.getValue(p.getKey())));
+                                        dataSet.add(values);
+                                    }
+                                    resultHandler.handle(Future.succeededFuture(dataSet));
+                                }
+                            });
+                        } while (cursor.hasMore());
+                        tx.commit();
+                        conn.close();
+                    } else {
+                        conn.close();
+                        LOGGER.error("Ошибка подготовки запроса: " + ar2.cause().getMessage());
+                        resultHandler.handle(Future.failedFuture(ar2.cause()));
                     }
-                    dataSet.add(values);
-                  }
-                  resultHandler.handle(Future.succeededFuture(dataSet));
-                }
-              });
-            } while (cursor.hasMore());
-            tx.commit();
-            conn.close();
-          } else {
-            conn.close();
-            LOGGER.error("Ошибка подготовки запроса: " + ar2.cause().getMessage());
-            resultHandler.handle(Future.failedFuture(ar2.cause()));
-          }
+                });
+            } else {
+                LOGGER.error("Ошибка подключения: " + ar1.cause().getMessage());
+                resultHandler.handle(Future.failedFuture(ar1.cause()));
+            }
         });
-      } else {
-        LOGGER.error("Ошибка подключения: " + ar1.cause().getMessage());
-        resultHandler.handle(Future.failedFuture(ar1.cause()));
-      }
-    });
-  }
+    }
 
-  @Override
-  public void executeUpdate(String sql, Handler<AsyncResult<Void>> completionHandler) {
-    pool.getConnection(ar1 -> {
-      if (ar1.succeeded()) {
-        PgConnection conn = ar1.result();
-        conn.query(sql, ar2 -> {
-          if (ar2.succeeded()) {
-            completionHandler.handle(Future.succeededFuture());
-          } else {
-            completionHandler.handle(Future.failedFuture(ar2.cause()));
-          }
-          conn.close();
+    @Override
+    public void executeUpdate(String sql, Handler<AsyncResult<Void>> completionHandler) {
+        pool.getConnection(ar1 -> {
+            if (ar1.succeeded()) {
+                PgConnection conn = ar1.result();
+                conn.query(sql, ar2 -> {
+                    if (ar2.succeeded()) {
+                        completionHandler.handle(Future.succeededFuture());
+                    } else {
+                        completionHandler.handle(Future.failedFuture(ar2.cause()));
+                    }
+                    conn.close();
+                });
+            } else {
+                LOGGER.error("Ошибка подключения: " + ar1.cause().getMessage());
+                completionHandler.handle(Future.failedFuture(ar1.cause()));
+            }
         });
-      } else {
-        LOGGER.error("Ошибка подключения: " + ar1.cause().getMessage());
-        completionHandler.handle(Future.failedFuture(ar1.cause()));
-      }
-    });
-  }
+    }
 
-  @Override
-  public void executeWithParams(String sql, List<Object> params, Handler<AsyncResult<?>> resultHandler) {
-    pool.getConnection(ar1 -> {
-      if (ar1.succeeded()) {
-        PgConnection conn = ar1.result();
-        conn.preparedQuery(sql, new ArrayTuple(params), ar2 -> {
-          if (ar2.succeeded()) {
-            resultHandler.handle(Future.succeededFuture(ar2.result()));
-          } else {
-            resultHandler.handle(Future.failedFuture(ar2.cause()));
-          }
+    @Override
+    public void executeWithParams(String sql, List<Object> params, Handler<AsyncResult<?>> resultHandler) {
+        pool.getConnection(ar1 -> {
+            if (ar1.succeeded()) {
+                PgConnection conn = ar1.result();
+                conn.preparedQuery(sql, new ArrayTuple(params), ar2 -> {
+                    if (ar2.succeeded()) {
+                        resultHandler.handle(Future.succeededFuture(ar2.result()));
+                    } else {
+                        resultHandler.handle(Future.failedFuture(ar2.cause()));
+                    }
+                });
+            } else {
+                LOGGER.error("Ошибка подключения: " + ar1.cause().getMessage());
+                resultHandler.handle(Future.failedFuture(ar1.cause()));
+            }
         });
-      } else {
-        LOGGER.error("Ошибка подключения: " + ar1.cause().getMessage());
-        resultHandler.handle(Future.failedFuture(ar1.cause()));
-      }
-    });
-  }
+    }
 
 }

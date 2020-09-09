661a2e1bf54630dab009137d5e88cc864506fb5d
VitekArkhipov <vitek.arkhipov@gmail.com>
2020-09-09 16:40:28 +0500

    DTM-478 implemented custom calcite framework and planner classes, implemented custom RelDataTypeSystem class for correct using option shouldConvertRaggedUnionTypesToVarying

--- dtm-query-execution-core/src/test/java/ru/ibs/dtm/query/execution/core/service/ddl/DropSchemaDdlExecutorTest.java
+++ dtm-query-execution-core/src/test/java/ru/ibs/dtm/query/execution/core/service/ddl/DropSchemaDdlExecutorTest.java
@@ -8,7 +8,6 @@ import org.apache.calcite.sql.SqlNode;
 import org.apache.calcite.sql.parser.SqlParseException;
 import org.apache.calcite.sql.parser.SqlParser;
 import org.apache.calcite.tools.FrameworkConfig;
-import org.apache.calcite.tools.Frameworks;
 import org.apache.calcite.tools.Planner;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,6 +17,7 @@ import ru.ibs.dtm.common.model.ddl.ClassTable;
 import ru.ibs.dtm.common.model.ddl.ColumnType;
 import ru.ibs.dtm.common.reader.QueryRequest;
 import ru.ibs.dtm.query.calcite.core.configuration.CalciteCoreConfiguration;
+import ru.ibs.dtm.query.calcite.core.framework.DtmCalciteFramework;
 import ru.ibs.dtm.query.calcite.core.service.DefinitionService;
 import ru.ibs.dtm.query.execution.core.configuration.calcite.CalciteConfiguration;
 import ru.ibs.dtm.query.execution.core.configuration.jooq.MariaProperties;
@@ -71,9 +71,9 @@ class DropSchemaDdlExecutorTest {
 
     @BeforeEach
     void setUp() throws SqlParseException {
-        Frameworks.ConfigBuilder configBuilder = Frameworks.newConfigBuilder();
+        DtmCalciteFramework.ConfigBuilder configBuilder = DtmCalciteFramework.newConfigBuilder();
         FrameworkConfig frameworkConfig = configBuilder.parserConfig(parserConfig).build();
-        planner = Frameworks.getPlanner(frameworkConfig);
+        planner = DtmCalciteFramework.getPlanner(frameworkConfig);
         dropSchemaDdlExecutor = new DropSchemaDdlExecutor(metadataExecutor,
                 mariaProperties, serviceDbFacade, definitionService);
         when(serviceDbFacade.getServiceDbDao()).thenReturn(serviceDbDao);

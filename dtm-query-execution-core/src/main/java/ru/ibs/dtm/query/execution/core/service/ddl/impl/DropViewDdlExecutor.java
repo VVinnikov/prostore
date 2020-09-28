8ceb246717ce8a0e1ad9b972a8b6049ae8ff3331
Ivan Lapa <lin@arenadata.io>
2020-09-28 21:34:13 +0300

    DTM-590 bugfix

--- dtm-query-execution-core/src/main/java/ru/ibs/dtm/query/execution/core/service/ddl/impl/DropViewDdlExecutor.java
+++ dtm-query-execution-core/src/main/java/ru/ibs/dtm/query/execution/core/service/ddl/impl/DropViewDdlExecutor.java
@@ -41,7 +41,7 @@ public class DropViewDdlExecutor extends QueryResultDdlExecutor {
                 .orElseThrow(() -> new RuntimeException("Unable to get schema of view"));
             val viewName = viewNameNode.tryGetTableName()
                 .orElseThrow(() -> new RuntimeException("Unable to get name of view"));
-            entityDao.getEntity(context.getDatamartName(), viewName)
+            entityDao.getEntity(schemaName, viewName)
                 .compose(this::checkEntityType)
                 .compose(v -> entityDao.deleteEntity(schemaName, viewName))
                 .onSuccess(success -> handler.handle(Future.succeededFuture(QueryResult.emptyResult())))

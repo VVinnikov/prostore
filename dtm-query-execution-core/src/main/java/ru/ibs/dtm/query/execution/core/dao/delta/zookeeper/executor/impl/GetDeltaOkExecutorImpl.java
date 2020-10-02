50c9ab73d4577876863d9ca69c1da1dcf7ab7c4e
Ivan Lapa <lin@arenadata.io>
2020-10-02 09:35:15 +0300

    DTM-627 implemented dao executor for function "get delta ok"

---
+++ dtm-query-execution-core/src/main/java/ru/ibs/dtm/query/execution/core/dao/delta/zookeeper/executor/impl/GetDeltaOkExecutorImpl.java
@@ -0,0 +1,57 @@
+package ru.ibs.dtm.query.execution.core.dao.delta.zookeeper.executor.impl;
+
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.data.Stat;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import ru.ibs.dtm.query.execution.core.dao.delta.zookeeper.executor.DeltaDaoExecutor;
+import ru.ibs.dtm.query.execution.core.dao.delta.zookeeper.executor.DeltaServiceDaoExecutorHelper;
+import ru.ibs.dtm.query.execution.core.dao.delta.zookeeper.executor.GetDeltaOkExecutor;
+import ru.ibs.dtm.query.execution.core.dao.exception.delta.DeltaException;
+import ru.ibs.dtm.query.execution.core.dao.exception.delta.DeltaNotFoundException;
+import ru.ibs.dtm.query.execution.core.dto.delta.Delta;
+import ru.ibs.dtm.query.execution.core.dto.delta.OkDelta;
+import ru.ibs.dtm.query.execution.core.service.zookeeper.ZookeeperExecutor;
+
+@Slf4j
+@Component
+public class GetDeltaOkExecutorImpl extends DeltaServiceDaoExecutorHelper implements GetDeltaOkExecutor {
+
+    public GetDeltaOkExecutorImpl(ZookeeperExecutor executor,
+                                     @Value("${core.env.name}") String envName) {
+        super(executor, envName);
+    }
+
+    @Override
+    public Future<OkDelta> execute(String datamart) {
+        val deltaStat = new Stat();
+        Promise<OkDelta> resultPromise = Promise.promise();
+        executor.getData(getDeltaPath(datamart), null, deltaStat)
+            .map(this::deserializedDelta)
+            .map(Delta::getOk)
+            .onSuccess(r -> {
+                log.debug("get delta ok by datamart[{}] completed successfully: [{}]", datamart, r);
+                resultPromise.complete(r);
+            })
+            .onFailure(error -> {
+                val errMsg = String.format("can't get delta ok on datamart[%s]",
+                    datamart);
+                log.error(errMsg, error);
+                if (error instanceof KeeperException.NoNodeException) {
+                    resultPromise.fail(new DeltaNotFoundException(error));
+                } else {
+                    resultPromise.fail(new DeltaException(errMsg, error));
+                }
+            });
+        return resultPromise.future();
+    }
+
+    @Override
+    public Class<? extends DeltaDaoExecutor> getExecutorInterface() {
+        return GetDeltaOkExecutor.class;
+    }
+}

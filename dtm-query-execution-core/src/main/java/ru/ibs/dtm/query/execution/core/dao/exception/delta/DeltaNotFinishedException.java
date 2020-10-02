bfedd881059f77f85b4a19314df12ed6da422234
Ivan Lapa <lin@arenadata.io>
2020-10-02 09:24:07 +0300

    DTM-627 added delta exceptions

---
+++ dtm-query-execution-core/src/main/java/ru/ibs/dtm/query/execution/core/dao/exception/delta/DeltaNotFinishedException.java
@@ -0,0 +1,13 @@
+package ru.ibs.dtm.query.execution.core.dao.exception.delta;
+
+public class DeltaNotFinishedException extends DeltaException {
+    private static final String MESSAGE = "Delta not finished write operation exist";
+
+    public DeltaNotFinishedException() {
+        super(MESSAGE);
+    }
+
+    public DeltaNotFinishedException(Throwable cause) {
+        super(MESSAGE, cause);
+    }
+}
---
+++ dtm-query-execution-core/src/main/java/ru/ibs/dtm/query/execution/core/dao/exception/delta/TableBlockedException.java
@@ -0,0 +1,9 @@
+package ru.ibs.dtm.query.execution.core.dao.exception.delta;
+
+public class TableBlockedException extends DeltaException {
+    private static final String MESSAGE = "Table[%s] blocked";
+
+    public TableBlockedException(String tableName, Throwable cause) {
+        super(String.format(MESSAGE, tableName), cause);
+    }
+}

47f0f637bff5dd36393bed62b39961778c7f825b
Valery <vbir94@gmail.com>
2020-10-05 11:56:59 +0400

    DTM-617 Added unit tests for eddl executors

---
+++ dtm-query-execution-core/src/test/java/ru/ibs/dtm/query/execution/core/service/eddl/CreateUploadExternalTableExecutorTest.java
@@ -0,0 +1,165 @@
+package ru.ibs.dtm.query.execution.core.service.eddl;
+
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import org.apache.avro.Schema;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import ru.ibs.dtm.common.model.ddl.ColumnType;
+import ru.ibs.dtm.common.model.ddl.Entity;
+import ru.ibs.dtm.common.model.ddl.EntityField;
+import ru.ibs.dtm.common.model.ddl.EntityType;
+import ru.ibs.dtm.common.plugin.exload.Format;
+import ru.ibs.dtm.common.plugin.exload.Type;
+import ru.ibs.dtm.query.execution.core.dao.ServiceDbFacade;
+import ru.ibs.dtm.query.execution.core.dao.ServiceDbFacadeImpl;
+import ru.ibs.dtm.query.execution.core.dao.servicedb.zookeeper.DatamartDao;
+import ru.ibs.dtm.query.execution.core.dao.servicedb.zookeeper.EntityDao;
+import ru.ibs.dtm.query.execution.core.dao.servicedb.zookeeper.ServiceDbDao;
+import ru.ibs.dtm.query.execution.core.dao.servicedb.zookeeper.impl.DatamartDaoImpl;
+import ru.ibs.dtm.query.execution.core.dao.servicedb.zookeeper.impl.EntityDaoImpl;
+import ru.ibs.dtm.query.execution.core.dao.servicedb.zookeeper.impl.ServiceDbDaoImpl;
+import ru.ibs.dtm.query.execution.core.dto.eddl.CreateUploadExternalTableQuery;
+import ru.ibs.dtm.query.execution.core.service.avro.AvroSchemaGenerator;
+import ru.ibs.dtm.query.execution.core.service.avro.AvroSchemaGeneratorImpl;
+import ru.ibs.dtm.query.execution.core.service.eddl.impl.CreateUploadExternalTableExecutor;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class CreateUploadExternalTableExecutorTest {
+
+    private final ServiceDbFacade serviceDbFacade = mock(ServiceDbFacadeImpl.class);
+    private final ServiceDbDao serviceDbDao = mock(ServiceDbDaoImpl.class);
+    private final DatamartDao datamartDao = mock(DatamartDaoImpl.class);
+    private final EntityDao entityDao = mock(EntityDaoImpl.class);
+    private final AvroSchemaGenerator avroSchemaGenerator = new AvroSchemaGeneratorImpl();
+    private EddlExecutor createUploadExteranlTableExecutor;
+    private CreateUploadExternalTableQuery query;
+    private String schema;
+    private Entity entity;
+
+    @BeforeEach
+    void setUp(){
+        when(serviceDbFacade.getServiceDbDao()).thenReturn(serviceDbDao);
+        when(serviceDbDao.getEntityDao()).thenReturn(entityDao);
+        when(serviceDbDao.getDatamartDao()).thenReturn(datamartDao);
+        createUploadExteranlTableExecutor = new CreateUploadExternalTableExecutor(serviceDbFacade);
+
+        schema = "shares";
+        String table = "accounts";
+        EntityField f1 = new EntityField(0, "id", ColumnType.INT, false);
+        EntityField f2 = new EntityField(1, "name", ColumnType.VARCHAR, true);
+        f2.setSize(20);
+        entity = new Entity(table, schema, Arrays.asList(f1, f2));
+        entity.setEntityType(EntityType.UPLOAD_EXTERNAL_TABLE);
+        Schema avroSchema = avroSchemaGenerator.generateTableSchema(entity, false);
+        int messageSize = 10;
+        String locationPath = "kafka://localhost:2181/KAFKA_TOPIC";
+        query = new CreateUploadExternalTableQuery(schema,
+                table,
+                entity,
+                Type.KAFKA_TOPIC,
+                locationPath,
+                Format.AVRO,
+                avroSchema.toString(),
+                messageSize);
+
+    }
+
+    @Test
+    void executeSuccess(){
+        Promise promise = Promise.promise();
+
+        Mockito.when(datamartDao.existsDatamart(eq(schema)))
+                .thenReturn(Future.succeededFuture(true));
+
+        Mockito.when(entityDao.existsEntity(eq(schema), eq(entity.getName())))
+                .thenReturn(Future.succeededFuture(false));
+
+        Mockito.when(entityDao.createEntity(any()))
+                .thenReturn(Future.succeededFuture());
+
+        createUploadExteranlTableExecutor.execute(query, ar -> {
+            if (ar.succeeded()) {
+                promise.complete(ar.result());
+            } else {
+                promise.fail(ar.cause());
+            }
+        });
+        assertTrue(promise.future().succeeded());
+        assertFalse(promise.future().failed());
+    }
+
+    @Test
+    void executeDatamartNotExists(){
+        Promise promise = Promise.promise();
+
+        Mockito.when(datamartDao.existsDatamart(eq(schema)))
+                .thenReturn(Future.succeededFuture(false));
+
+        createUploadExteranlTableExecutor.execute(query, ar -> {
+            if (ar.succeeded()) {
+                promise.complete(ar.result());
+            } else {
+                promise.fail(ar.cause());
+            }
+        });
+        assertFalse(promise.future().succeeded());
+        assertTrue(promise.future().failed());
+        assertNotNull(promise.future().cause());
+    }
+
+    @Test
+    void executeTableExists(){
+        Promise promise = Promise.promise();
+
+        Mockito.when(datamartDao.existsDatamart(eq(schema)))
+                .thenReturn(Future.succeededFuture(true));
+
+        Mockito.when(entityDao.existsEntity(eq(schema), eq(entity.getName())))
+                .thenReturn(Future.failedFuture("exists entity error"));
+
+        createUploadExteranlTableExecutor.execute(query, ar -> {
+            if (ar.succeeded()) {
+                promise.complete(ar.result());
+            } else {
+                promise.fail(ar.cause());
+            }
+        });
+        assertFalse(promise.future().succeeded());
+        assertTrue(promise.future().failed());
+        assertNotNull(promise.future().cause());
+    }
+
+    @Test
+    void executeTableNotExistsError(){
+        Promise promise = Promise.promise();
+
+        Mockito.when(datamartDao.existsDatamart(eq(schema)))
+                .thenReturn(Future.succeededFuture(true));
+
+        Mockito.when(entityDao.existsEntity(eq(schema), eq(entity.getName())))
+                .thenReturn(Future.succeededFuture(false));
+
+        Mockito.when(entityDao.createEntity(any()))
+                .thenReturn(Future.failedFuture("creating new entity error"));
+
+        createUploadExteranlTableExecutor.execute(query, ar -> {
+            if (ar.succeeded()) {
+                promise.complete(ar.result());
+            } else {
+                promise.fail(ar.cause());
+            }
+        });
+        assertFalse(promise.future().succeeded());
+        assertTrue(promise.future().failed());
+        assertNotNull(promise.future().cause());
+    }
+}

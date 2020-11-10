9cfb1ecd86568f2206138fff12087a036ffb9452
Ivan Lapa <lin@arenadata.io>
2020-11-10 13:21:51 +0300

    DTM-737 refactoring cache services

--- dtm-query-execution-core/src/main/java/io/arenadata/dtm/query/execution/core/service/cache/AbstractCacheService.java
+++ dtm-query-execution-core/src/main/java/io/arenadata/dtm/query/execution/core/service/cache/AbstractCacheService.java
@@ -5,21 +5,15 @@ import lombok.val;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 
-import java.util.HashSet;
 import java.util.Objects;
-import java.util.Set;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public abstract class AbstractCacheService<K, V> implements CacheService<K, V> {
     protected final String cacheConfiguration;
     protected final CacheManager cacheManager;
-    private final Set<K> keys;
 
     public AbstractCacheService(String cacheConfiguration, CacheManager cacheManager) {
         this.cacheConfiguration = cacheConfiguration;
         this.cacheManager = cacheManager;
-        this.keys = new HashSet<>();
     }
 
     @Override
@@ -46,30 +40,17 @@ public abstract class AbstractCacheService<K, V> implements CacheService<K, V> {
 
     @Override
     public Future<V> put(K key, V value) {
-        final Future<V> deltaFuture = Future.succeededFuture(value);
-        Objects.requireNonNull(cacheManager.getCache(cacheConfiguration)).put(key, deltaFuture);
-        keys.add(key);
-        return deltaFuture;
+        return Future.succeededFuture(value);
     }
 
     @Override
     public void remove(K key) {
         Cache cache = cacheManager.getCache(cacheConfiguration);
         Objects.requireNonNull(cache).evict(key);
-        keys.remove(key);
     }
 
     @Override
     public void clear() {
         Objects.requireNonNull(cacheManager.getCache(cacheConfiguration)).clear();
-        keys.clear();
-    }
-
-    @Override
-    public void removeIf(Predicate<K> removeCondition) {
-        val byRemove = keys.stream()
-                .filter(removeCondition)
-                .collect(Collectors.toSet());
-        byRemove.forEach(this::remove);
     }
 }
--- dtm-query-execution-core/src/main/java/io/arenadata/dtm/query/execution/core/service/cache/impl/EntityCacheServiceImpl.java
+++ dtm-query-execution-core/src/main/java/io/arenadata/dtm/query/execution/core/service/cache/impl/EntityCacheServiceImpl.java
@@ -2,7 +2,7 @@ package io.arenadata.dtm.query.execution.core.service.cache.impl;
 
 import io.arenadata.dtm.common.model.ddl.Entity;
 import io.arenadata.dtm.query.execution.core.configuration.cache.CacheConfiguration;
-import io.arenadata.dtm.query.execution.core.service.cache.AbstractCacheService;
+import io.arenadata.dtm.query.execution.core.service.cache.AbstractCaffeineCacheService;
 import io.arenadata.dtm.query.execution.core.service.cache.EntityCacheService;
 import io.arenadata.dtm.query.execution.core.service.cache.key.EntityKey;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +11,7 @@ import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Component;
 
 @Component("entityCacheService")
-public class EntityCacheServiceImpl extends AbstractCacheService<EntityKey, Entity> implements EntityCacheService {
+public class EntityCacheServiceImpl extends AbstractCaffeineCacheService<EntityKey, Entity> implements EntityCacheService {
 
     @Autowired
     public EntityCacheServiceImpl(@Qualifier("caffeineCacheManager") CacheManager cacheManager) {

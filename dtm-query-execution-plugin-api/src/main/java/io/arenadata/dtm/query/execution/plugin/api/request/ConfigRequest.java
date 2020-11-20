404fac6c9990bbffda6d59036012f88ae96f172a
Ivan Lapa <lin@arenadata.io>
2020-11-20 08:32:59 +0300

    DTM-783 Plugin API: add interfaces for ConfigExecutor, ConfigService

---
+++ dtm-query-execution-plugin-api/src/main/java/io/arenadata/dtm/query/execution/plugin/api/request/ConfigRequest.java
@@ -0,0 +1,11 @@
+package io.arenadata.dtm.query.execution.plugin.api.request;
+
+import io.arenadata.dtm.common.reader.QueryRequest;
+
+
+public class ConfigRequest extends DatamartRequest {
+
+    public ConfigRequest(QueryRequest queryRequest) {
+        super(queryRequest);
+    }
+}

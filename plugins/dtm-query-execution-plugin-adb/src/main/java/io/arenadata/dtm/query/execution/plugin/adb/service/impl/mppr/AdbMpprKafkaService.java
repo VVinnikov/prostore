52fbe692dd8304be0e144d958b9056e046b22c92
Valery <vbir94@gmail.com>
2020-11-23 19:03:31 +0400

    DTM-726 Add error log

--- plugins/dtm-query-execution-plugin-adb/src/main/java/io/arenadata/dtm/query/execution/plugin/adb/service/impl/mppr/AdbMpprKafkaService.java
+++ plugins/dtm-query-execution-plugin-adb/src/main/java/io/arenadata/dtm/query/execution/plugin/adb/service/impl/mppr/AdbMpprKafkaService.java
@@ -43,6 +43,7 @@ public class AdbMpprKafkaService implements MpprKafkaService<QueryResult> {
 				.compose(v -> dropWritableExtTable(schema, table))
 				.onSuccess(success -> asyncHandler.handle(Future.succeededFuture(QueryResult.emptyResult())))
 				.onFailure(err -> {
+					log.error("Failed to unload data from ADB: %s on request %s", err.getMessage(), request.getQueryRequest().getRequestId());
 					dropWritableExtTable(schema, table)
 							.onComplete(dropResult -> {
 								if (dropResult.failed()) {

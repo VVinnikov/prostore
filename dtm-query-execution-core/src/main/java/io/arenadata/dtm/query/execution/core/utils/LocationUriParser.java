4eff287dfe8abbe88f9b59488b6232d951b5c4cf
Valery <vbir94@gmail.com>
2020-11-25 15:12:33 +0400

    DTM-795 Change external tables location parsing

--- dtm-query-execution-core/src/main/java/io/arenadata/dtm/query/execution/core/utils/LocationUriParser.java
+++ dtm-query-execution-core/src/main/java/io/arenadata/dtm/query/execution/core/utils/LocationUriParser.java
@@ -3,23 +3,34 @@ package io.arenadata.dtm.query.execution.core.utils;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 
 import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class LocationUriParser {
 
     public static final int DEFAULT_ZOOKEEPER_PORT = 2181;
     private static final String HOST_DELIMITER = ":";
+    private static final String HOSTS_DELIMITER = ",";
 
     public static KafkaTopicUri parseKafkaLocationPath(String locationPath) {
         try {
             URI uri = URI.create(locationPath);
-            String topic = uri.getPath().substring(1);
-            String[] authorityArray = uri.getAuthority().split(HOST_DELIMITER);
-            String host = authorityArray[0];
-            int port = authorityArray.length > 1 ? Integer.parseInt(authorityArray[1]) : DEFAULT_ZOOKEEPER_PORT;
-            return new KafkaTopicUri(host, topic, port);
+            val lastSlashIdx = uri.getPath().lastIndexOf("/");
+            val topic = uri.getPath().substring(lastSlashIdx + 1);
+            val chroot = uri.getPath().substring(0, lastSlashIdx);
+            val hosts = uri.getAuthority().split(HOSTS_DELIMITER);
+            List<String> hostArray = Arrays.stream(hosts).map(hostPort -> {
+                val hostPortArray = hostPort.split(HOST_DELIMITER);
+                val host = hostPortArray[0];
+                val port = hostPortArray.length > 1 ? Integer.parseInt(hostPortArray[1]) : DEFAULT_ZOOKEEPER_PORT;
+                return host + HOST_DELIMITER + port;
+            }).collect(Collectors.toList());
+            return new KafkaTopicUri(hostArray, chroot, topic);
         } catch (Exception e) {
             String errMsg = String.format("LocationPath parsing error [%s]: %s", locationPath, e.getMessage());
             log.error(errMsg, e);
@@ -30,12 +41,12 @@ public class LocationUriParser {
     @Data
     @AllArgsConstructor
     public final static class KafkaTopicUri {
-        private String host;
+        private List<String> hosts;
+        private String chroot;
         private String topic;
-        private int port;
 
         public String getAddress() {
-            return this.host + HOST_DELIMITER + this.port;
+            return String.join(HOSTS_DELIMITER, this.hosts) + this.chroot;
         }
     }
 }
--- dtm-query-execution-core/src/test/java/io/arenadata/dtm/query/execution/core/factory/impl/LocationUriParserTest.java
+++ dtm-query-execution-core/src/test/java/io/arenadata/dtm/query/execution/core/factory/impl/LocationUriParserTest.java
@@ -11,30 +11,36 @@ class LocationUriParserTest {
 
   public static final String LOCATION_PATH_WITHOUT_PORT = "kafka://localhost/topicX";
   public static final String LOCATION_PATH = "kafka://localhost:2181/topicX";
-  public static final String EXPECTED_HOST = "localhost";
+  public static final String LOCATION_PATH_WITH_MULTIPLE_HOSTS = "kafka://localhost1:2181,localhost2:2181/chroot/topicX";
+  public static final String EXPECTED_ADDRESS_MULTIPLE_HOSTS = "localhost1:2181,localhost2:2181/chroot";
+  public static final String EXPECTED_ADDRESS = "localhost:2181";
   public static final String EXPECTED_TOPIC = "topicX";
-  public static final int EXPECTED_PORT = 2181;
 
   @Test
   void parseLocationPathWithZookeeperPort() {
     val topicUri = LocationUriParser.parseKafkaLocationPath(LOCATION_PATH);
-    assertEquals(EXPECTED_HOST, topicUri.getHost());
+    assertEquals(EXPECTED_ADDRESS, topicUri.getAddress());
     assertEquals(EXPECTED_TOPIC, topicUri.getTopic());
-    assertEquals(EXPECTED_PORT, topicUri.getPort());
   }
 
   @Test
   void parseLocationPathWithoutZookeeperPort() {
     val topicUri = LocationUriParser.parseKafkaLocationPath(LOCATION_PATH_WITHOUT_PORT);
-    assertEquals(EXPECTED_HOST, topicUri.getHost());
+    assertEquals(EXPECTED_ADDRESS, topicUri.getAddress());
     assertEquals(EXPECTED_TOPIC, topicUri.getTopic());
-    assertEquals(EXPECTED_PORT, topicUri.getPort());
+  }
+
+  @Test
+  void parseLocationPathWithMultipleHosts() {
+    val topicUri = LocationUriParser.parseKafkaLocationPath(LOCATION_PATH_WITH_MULTIPLE_HOSTS);
+    assertEquals(EXPECTED_ADDRESS_MULTIPLE_HOSTS, topicUri.getAddress());
+    assertEquals(EXPECTED_TOPIC, topicUri.getTopic());
   }
 
   @Test
   void badParseLocationPathWithoutZookeeperPort() {
     assertThrows(RuntimeException.class,
       () -> LocationUriParser.parseKafkaLocationPath("LOCATION_PATH_WITHOUT_PORT"),
-      "Ошибка парсинга locationPath [LOCATION_PATH_WITHOUT_PORT]: null");
+      "Parsing error locationPath [LOCATION_PATH_WITHOUT_PORT]: null");
   }
 }

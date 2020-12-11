45fd4e40746c9e97ae02827ead2e2ec465126741
Grigory Sidorov <grigorysidorov@gmail.com>
2020-12-11 17:33:48 +0300

    fix tests

--- plugins/dtm-query-execution-plugin-adb/src/test/java/io/arenadata/dtm/query/execution/plugin/adb/factory/impl/AdbCreateTableQueriesFactoryTest.java
+++ plugins/dtm-query-execution-plugin-adb/src/test/java/io/arenadata/dtm/query/execution/plugin/adb/factory/impl/AdbCreateTableQueriesFactoryTest.java
@@ -16,7 +16,7 @@ public class AdbCreateTableQueriesFactoryTest {
     private static final String EXPECTED_CREATE_ACTUAL_TABLE_QUERY = "CREATE TABLE test_schema.test_table_actual " +
             "(id int8 NOT NULL, sk_key2 int8 NOT NULL, pk2 int8 NOT NULL, sk_key3 int8 NOT NULL, " +
             "VARCHAR_type varchar(20), CHAR_type varchar(20), BIGINT_type int8, INT_type int8, DOUBLE_type float8, " +
-            "FLOAT_type float4, DATE_type date, TIME_type time(5), TIMESTAMP_type timestamp(5), BOOLEAN_type bool, " +
+            "FLOAT_type float4, DATE_type date, TIME_type time(6), TIMESTAMP_type timestamp(6), BOOLEAN_type bool, " +
             "UUID_type varchar(36), sys_from int8, sys_to int8, sys_op int4, " +
             "constraint pk_test_schema_test_table_actual primary key (id, pk2, sys_from)) " +
             "DISTRIBUTED BY (id, sk_key2, sk_key3)";
@@ -24,7 +24,7 @@ public class AdbCreateTableQueriesFactoryTest {
     private static final String EXPECTED_CREATE_HISTORY_TABLE_QUERY = "CREATE TABLE test_schema.test_table_history " +
             "(id int8 NOT NULL, sk_key2 int8 NOT NULL, pk2 int8 NOT NULL, sk_key3 int8 NOT NULL, " +
             "VARCHAR_type varchar(20), CHAR_type varchar(20), BIGINT_type int8, INT_type int8, DOUBLE_type float8, " +
-            "FLOAT_type float4, DATE_type date, TIME_type time(5), TIMESTAMP_type timestamp(5), BOOLEAN_type bool, " +
+            "FLOAT_type float4, DATE_type date, TIME_type time(6), TIMESTAMP_type timestamp(6), BOOLEAN_type bool, " +
             "UUID_type varchar(36), sys_from int8, sys_to int8, sys_op int4, " +
             "constraint pk_test_schema_test_table_history primary key (id, pk2, sys_from)) " +
             "DISTRIBUTED BY (id, sk_key2, sk_key3)";
@@ -32,7 +32,7 @@ public class AdbCreateTableQueriesFactoryTest {
     private static final String EXPECTED_CREATE_STAGING_TABLE_QUERY = "CREATE TABLE test_schema.test_table_staging " +
             "(id int8 NOT NULL, sk_key2 int8 NOT NULL, pk2 int8 NOT NULL, sk_key3 int8 NOT NULL, " +
             "VARCHAR_type varchar(20), CHAR_type varchar(20), BIGINT_type int8, INT_type int8, DOUBLE_type float8, " +
-            "FLOAT_type float4, DATE_type date, TIME_type time(5), TIMESTAMP_type timestamp(5), BOOLEAN_type bool, " +
+            "FLOAT_type float4, DATE_type date, TIME_type time(6), TIMESTAMP_type timestamp(6), BOOLEAN_type bool, " +
             "UUID_type varchar(36), sys_from int8, sys_to int8, sys_op int4, req_id varchar(36), " +
             "constraint pk_test_schema_test_table_staging primary key (id, pk2)) " +
             "DISTRIBUTED BY (id, sk_key2, sk_key3)";

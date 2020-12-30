57178707491cdfdeb0d0f78381f35801686b4bfc
Grigory Sidorov <grigorysidorov@gmail.com>
2020-12-30 11:08:29 +0300

    fix test

--- plugins/dtm-query-execution-plugin-adb/src/test/java/io/arenadata/dtm/query/execution/plugin/adb/service/impl/dml/AdbLlrServiceTest.java
+++ plugins/dtm-query-execution-plugin-adb/src/test/java/io/arenadata/dtm/query/execution/plugin/adb/service/impl/dml/AdbLlrServiceTest.java
@@ -6,6 +6,8 @@ import io.arenadata.dtm.common.cache.QueryTemplateKey;
 import io.arenadata.dtm.common.cache.QueryTemplateValue;
 import io.arenadata.dtm.common.metrics.RequestMetrics;
 import io.arenadata.dtm.common.reader.QueryRequest;
+import io.arenadata.dtm.common.reader.QueryResult;
+import io.arenadata.dtm.common.reader.QueryTemplateResult;
 import io.arenadata.dtm.query.calcite.core.dialect.LimitSqlDialect;
 import io.arenadata.dtm.query.calcite.core.service.QueryTemplateExtractor;
 import io.arenadata.dtm.query.calcite.core.service.impl.QueryTemplateExtractorImpl;
@@ -17,47 +19,49 @@ import io.arenadata.dtm.query.execution.plugin.api.request.LlrRequest;
 import io.arenadata.dtm.query.execution.plugin.api.service.LlrService;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
-import io.vertx.core.json.JsonObject;
-import io.vertx.ext.unit.Async;
-import io.vertx.ext.unit.TestOptions;
-import io.vertx.ext.unit.TestSuite;
-import io.vertx.ext.unit.report.ReportOptions;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.calcite.avatica.util.Casing;
 import org.apache.calcite.sql.SqlDialect;
+import org.apache.calcite.sql.SqlNode;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import utils.JsonUtils;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @Slf4j
 class AdbLlrServiceTest {
+    private final static String template = "SELECT * from PSO";
+    private LlrService<QueryResult> adbLLRService;
 
-    private final LlrService adbLLRService;
-    private final QueryEnrichmentService adbQueryEnrichmentService = mock(QueryEnrichmentService.class);
-    private final DatabaseExecutor adbDatabaseExecutor = mock(DatabaseExecutor.class);
-    private final CacheService<QueryTemplateKey, QueryTemplateValue> queryCacheService = mock(CaffeineCacheService.class);
-    private final QueryTemplateExtractor queryTemplateExtractor = mock(QueryTemplateExtractorImpl.class);
-
-    public AdbLlrServiceTest() {
-        //Моки для успешного исполнения
+    @BeforeEach
+    void init() {
         AsyncResult<Void> asyncResultEmpty = mock(AsyncResult.class);
         when(asyncResultEmpty.succeeded()).thenReturn(true);
 
         AsyncResult<List<List<?>>> asyncResult = mock(AsyncResult.class);
         when(asyncResult.succeeded()).thenReturn(true);
         when(asyncResult.result()).thenReturn(new ArrayList<>());
+        QueryEnrichmentService adbQueryEnrichmentService = mock(QueryEnrichmentService.class);
         when(adbQueryEnrichmentService.enrich(any()))
-                .thenReturn(Future.succeededFuture());
+                .thenReturn(Future.succeededFuture(template));
+        DatabaseExecutor adbDatabaseExecutor = mock(DatabaseExecutor.class);
         when(adbDatabaseExecutor.execute(any(), any()))
                 .thenReturn(Future.succeededFuture(new ArrayList<>()));
+        QueryTemplateResult queryTemplateResult = mock(QueryTemplateResult.class);
+        when(queryTemplateResult.getTemplate()).thenReturn(template);
+        QueryTemplateExtractor queryTemplateExtractor = mock(QueryTemplateExtractorImpl.class);
+        when(queryTemplateExtractor.extract(anyString(), any())).thenReturn(queryTemplateResult);
+        when(queryTemplateExtractor.extract(any(SqlNode.class), any())).thenReturn(queryTemplateResult);
+        CacheService<QueryTemplateKey, QueryTemplateValue> queryCacheService = mock(CaffeineCacheService.class);
+        when(queryCacheService.put(any(), any())).thenReturn(Future.succeededFuture());
         adbLLRService = new AdbLlrService(adbQueryEnrichmentService,
                 adbDatabaseExecutor,
                 queryCacheService,
@@ -72,22 +76,23 @@ class AdbLlrServiceTest {
 
     @Test
     void executeQuery() {
-        List<String> result = new ArrayList<>();
-        TestSuite suite = TestSuite.create("the_test_suite");
-        suite.test("executeQuery", context -> {
-            Async async = context.async();
-            JsonObject jsonSchema = JsonUtils.init("meta_data.json", "TEST_DATAMART");
-            List<Datamart> schema = new ArrayList<>();
-            schema.add(jsonSchema.mapTo(Datamart.class));
-            QueryRequest queryRequest = new QueryRequest();
-            queryRequest.setSql("SELECT * from PSO");
-            queryRequest.setRequestId(UUID.randomUUID());
-            queryRequest.setDatamartMnemonic("TEST_DATAMART");
-            LlrRequest llrRequest = new LlrRequest(queryRequest, schema, Collections.emptyList());
-            adbLLRService.execute(new LlrRequestContext(new RequestMetrics(), llrRequest));
-            async.awaitSuccess(7000);
-        });
-        suite.run(new TestOptions().addReporter(new ReportOptions().setTo("console")));
-        log.info(result.get(0));
+        List<Datamart> schema = Collections.singletonList(
+                new Datamart("TEST_DATAMART", false, Collections.emptyList()));
+        QueryRequest queryRequest = new QueryRequest();
+        queryRequest.setSql(template);
+        UUID uuid = UUID.randomUUID();
+        queryRequest.setRequestId(uuid);
+        queryRequest.setDatamartMnemonic("TEST_DATAMART");
+        QueryTemplateResult queryTemplateResult = new QueryTemplateResult(template,
+                Collections.emptyList());
+
+        LlrRequest llrRequest = new LlrRequest(queryRequest, schema, Collections.emptyList());
+        llrRequest.setSourceQueryTemplateResult(queryTemplateResult);
+        adbLLRService.execute(new LlrRequestContext(new RequestMetrics(), llrRequest))
+                .onComplete(ar -> {
+                    assertTrue(ar.succeeded());
+                    QueryResult result = ar.result();
+                    assertEquals(uuid, result.getRequestId());
+                });
     }
 }

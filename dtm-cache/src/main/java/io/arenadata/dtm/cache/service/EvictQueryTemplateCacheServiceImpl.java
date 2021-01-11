8ecdff91411af8a08e858a08e8f204cdc995c5d2
Grigory Sidorov <grigorysidorov@gmail.com>
2021-01-11 21:06:38 +0300

    DTM-874: Evict LL-R caches

--- dtm-cache/src/main/java/io/arenadata/dtm/cache/service/EvictQueryTemplateCacheServiceImpl.java
+++ dtm-cache/src/main/java/io/arenadata/dtm/cache/service/EvictQueryTemplateCacheServiceImpl.java
@@ -4,18 +4,14 @@ import io.arenadata.dtm.common.cache.QueryTemplateKey;
 import io.arenadata.dtm.common.cache.QueryTemplateValue;
 import io.arenadata.dtm.common.cache.SourceQueryTemplateValue;
 import io.arenadata.dtm.query.execution.model.metadata.Datamart;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.function.Predicate;
 
-@Service("evictQueryTemplateCacheServiceImpl")
 public class EvictQueryTemplateCacheServiceImpl implements EvictQueryTemplateCacheService {
     private final CacheService<QueryTemplateKey, SourceQueryTemplateValue> cacheService;
     private final List<CacheService<QueryTemplateKey, QueryTemplateValue>> cacheServiceList;
 
-    @Autowired
     public EvictQueryTemplateCacheServiceImpl(CacheService<QueryTemplateKey, SourceQueryTemplateValue> cacheService,
                                               List<CacheService<QueryTemplateKey, QueryTemplateValue>> cacheServiceList) {
         this.cacheService = cacheService;

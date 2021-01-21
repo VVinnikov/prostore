d658b59e86dea0e86c1941afe851a91039184402
Valery <vbir94@gmail.com>
2021-01-21 18:39:54 +0400

    DTM-895 Fix tests in ADG

--- plugins/dtm-query-execution-plugin-adg/src/test/java/io/arenadata/dtm/query/execution/plugin/adg/service/DtmDataSourcePluginIT.java
+++ plugins/dtm-query-execution-plugin-adg/src/test/java/io/arenadata/dtm/query/execution/plugin/adg/service/DtmDataSourcePluginIT.java
@@ -8,7 +8,6 @@ import io.arenadata.dtm.common.reader.QueryResult;
 import io.arenadata.dtm.common.reader.SourceType;
 import io.arenadata.dtm.query.execution.plugin.api.DtmDataSourcePlugin;
 import io.arenadata.dtm.query.execution.plugin.api.check.CheckTableRequest;
-import io.arenadata.dtm.query.execution.plugin.api.check.CheckTableRequest;
 import io.arenadata.dtm.query.execution.plugin.api.dto.CheckDataByCountRequest;
 import io.arenadata.dtm.query.execution.plugin.api.dto.CheckDataByHashInt32Request;
 import io.arenadata.dtm.query.execution.plugin.api.dto.RollbackRequest;
@@ -33,107 +32,107 @@ import java.util.Collections;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
-// FixMe Test
+
 @SpringBootTest(classes = DtmTestConfiguration.class)
 @ExtendWith(VertxExtension.class)
 class DtmDataSourcePluginIT {
-//
-//    @Autowired
-//    private DdlService ddlService;
-//
-//    private final DtmDataSourcePlugin plugin = new DtmDataSourcePlugin() {
-//
-//        @Override
-//        public boolean supports(SourceType sourceType) {
-//            return false;
-//        }
-//
-//        @Override
-//        public SourceType getSourceType() {
-//            return SourceType.ADG;
-//        }
-//
-//        @Override
-//        public Future<Void> ddl(DdlRequest request) {
-//            return null;
-//        }
-//
-//        @Override
-//        public Future<QueryResult> llr(LlrRequest context) {
-//            return null;
-//        }
-//
-//        @Override
-//        public Future<QueryResult> mppr(MpprRequest request) {
-//            return null;
-//        }
-//
-//        @Override
-//        public Future<QueryResult> mppw(MppwRequest request) {
-//            return null;
-//        }
-//
-//        @Override
-//        public Future<Integer> calcQueryCost(QueryCostRequest request) {
-//            return null;
-//        }
-//
-//        @Override
-//        public Future<StatusQueryResult> status(String topic) {
-//            return null;
-//        }
-//
-//        @Override
-//        public Future<Void> rollback(RollbackRequest request) {
-//            return null;
-//        }
-//
-//        @Override
-//        public Set<String> getActiveCaches() {
-//            return Collections.singleton("adg_datamart");
-//        }
-//
-//        @Override
-//        public Future<Void> checkTable(CheckTableRequest request) {
-//            return null;
-//        }
-//
-//        @Override
-//        public Future<Long> checkDataByCount(CheckDataByCountRequest request) {
-//            return null;
-//        }
-//
-//        @Override
-//        public Future<Long> checkDataByHashInt32(CheckDataByHashInt32Request params) {
-//            return null;
-//        }
-//
-//        @Override
-//        public Future<Void> truncateHistory(TruncateHistoryRequest params) {
-//            return null;
-//        }
-//    };
-//
-//    @Test
-//    void testDdl(VertxTestContext testContext) throws Throwable {
-//        Entity entity = new Entity("test.test_", Arrays.asList(
-//                new EntityField(0, "id", ColumnType.INT.name(), false, 1, 1, null),
-//                new EntityField(1, "test", ColumnType.VARCHAR.name(), true, 1, 1, null)
-//        ));
-//        DdlRequest request = new DdlRequest(UUID.randomUUID(),
-//                "test",
-//                "test",
-//                entity,
-//                SqlKind.CREATE_TABLE);
-//        plugin.ddl(request)
-//                .onComplete(ar -> {
-//                    if (ar.succeeded()) {
-//                        testContext.completeNow();
-//                    } else {
-//                        testContext.failNow(ar.cause());
-//                    }
-//                });
-//        testContext.awaitCompletion(5, TimeUnit.SECONDS);
-//    }
+
+    @Autowired
+    private DdlService ddlService;
+
+    private final DtmDataSourcePlugin plugin = new DtmDataSourcePlugin() {
+
+        @Override
+        public boolean supports(SourceType sourceType) {
+            return false;
+        }
+
+        @Override
+        public SourceType getSourceType() {
+            return SourceType.ADG;
+        }
+
+        @Override
+        public Future<Void> ddl(DdlRequest request) {
+            return null;
+        }
+
+        @Override
+        public Future<QueryResult> llr(LlrRequest context) {
+            return null;
+        }
+
+        @Override
+        public Future<QueryResult> mppr(MpprRequest request) {
+            return null;
+        }
+
+        @Override
+        public Future<QueryResult> mppw(MppwRequest request) {
+            return null;
+        }
+
+        @Override
+        public Future<Integer> calcQueryCost(QueryCostRequest request) {
+            return null;
+        }
+
+        @Override
+        public Future<StatusQueryResult> status(String topic) {
+            return null;
+        }
+
+        @Override
+        public Future<Void> rollback(RollbackRequest request) {
+            return null;
+        }
+
+        @Override
+        public Set<String> getActiveCaches() {
+            return Collections.singleton("adg_datamart");
+        }
+
+        @Override
+        public Future<Void> checkTable(CheckTableRequest request) {
+            return null;
+        }
+
+        @Override
+        public Future<Long> checkDataByCount(CheckDataByCountRequest request) {
+            return null;
+        }
+
+        @Override
+        public Future<Long> checkDataByHashInt32(CheckDataByHashInt32Request params) {
+            return null;
+        }
+
+        @Override
+        public Future<Void> truncateHistory(TruncateHistoryRequest params) {
+            return null;
+        }
+    };
+
+    @Test
+    void testDdl(VertxTestContext testContext) throws Throwable {
+        Entity entity = new Entity("test.test_", Arrays.asList(
+                new EntityField(0, "id", ColumnType.INT.name(), false, 1, 1, null),
+                new EntityField(1, "test", ColumnType.VARCHAR.name(), true, 1, 1, null)
+        ));
+        DdlRequest request = new DdlRequest(UUID.randomUUID(),
+                "test",
+                "test",
+                entity,
+                SqlKind.CREATE_TABLE);
+        plugin.ddl(request)
+                .onComplete(ar -> {
+                    if (ar.succeeded()) {
+                        testContext.completeNow();
+                    } else {
+                        testContext.failNow(ar.cause());
+                    }
+                });
+        testContext.awaitCompletion(5, TimeUnit.SECONDS);
+    }
 
 }
--- plugins/dtm-query-execution-plugin-adg/src/test/java/io/arenadata/dtm/query/execution/plugin/adg/service/impl/AdgCartridgeSchemaGeneratorImplIT.java
+++ plugins/dtm-query-execution-plugin-adg/src/test/java/io/arenadata/dtm/query/execution/plugin/adg/service/impl/AdgCartridgeSchemaGeneratorImplIT.java
@@ -22,57 +22,60 @@ import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.fail;
-//FixMe Test
+
 @SpringBootTest
 @ExtendWith(VertxExtension.class)
 class AdgCartridgeSchemaGeneratorImplIT {
-//
-//    @Autowired
-//    private AdgCartridgeSchemaGenerator generator;
-//
-//    @Autowired
-//    private AdgCartridgeClient client;
-//
-//    @Autowired
-//    @Qualifier("yamlMapper")
-//    private ObjectMapper yamlMapper;
-//
-//    private Entity entity = new Entity("test.test_", Arrays.asList(
-//            new EntityField(0, "id", ColumnType.INT.name(), false, 1, 1, null),
-//            new EntityField(1, "test", ColumnType.VARCHAR.name(), true, null, null, null)
-//    ));
-//
-//    @Test
-//    @SneakyThrows
-//    void generate(VertxTestContext testContext) throws Throwable {
-//        client.getSchema()
-//                .onComplete(ar1 -> {
-//                    if (ar1.succeeded()) {
-//                        OperationYaml yaml = parseYaml(ar1.result().getData().getCluster().getSchema().getYaml());
-//                        DdlRequestContext context = new DdlRequestContext(new DdlRequest(null, entity));
-//                        generator.generate(context, yaml)
-//                                .onComplete(ar2 -> {
-//                                    if (ar2.succeeded()) {
-//                                        testContext.completeNow();
-//                                    } else {
-//                                        testContext.failNow(ar2.cause());
-//                                    }
-//                                });
-//                    } else {
-//                        testContext.failNow(ar1.cause());
-//                    }
-//                });
-//        testContext.awaitCompletion(5, TimeUnit.SECONDS);
-//    }
-//
-//    private OperationYaml parseYaml(String yaml) {
-//        OperationYaml operationYaml = null;
-//        try {
-//            operationYaml = yamlMapper.readValue(yaml,
-//                    OperationYaml.class);
-//        } catch (JsonProcessingException e) {
-//            fail(e);
-//        }
-//        return operationYaml;
-//    }
+
+    @Autowired
+    private AdgCartridgeSchemaGenerator generator;
+
+    @Autowired
+    private AdgCartridgeClient client;
+
+    @Autowired
+    @Qualifier("yamlMapper")
+    private ObjectMapper yamlMapper;
+
+    private Entity entity = new Entity("test.test_", Arrays.asList(
+            new EntityField(0, "id", ColumnType.INT.name(), false, 1, 1, null),
+            new EntityField(1, "test", ColumnType.VARCHAR.name(), true, null, null, null)
+    ));
+
+    @Test
+    @SneakyThrows
+    void generate(VertxTestContext testContext) {
+        client.getSchema()
+                .onComplete(ar1 -> {
+                    if (ar1.succeeded()) {
+                        OperationYaml yaml = parseYaml(ar1.result().getData().getCluster().getSchema().getYaml());
+                        DdlRequest request = DdlRequest.builder()
+                                .envName("env")
+                                .entity(entity)
+                                .build();
+                        generator.generate(request, yaml)
+                                .onComplete(ar2 -> {
+                                    if (ar2.succeeded()) {
+                                        testContext.completeNow();
+                                    } else {
+                                        testContext.failNow(ar2.cause());
+                                    }
+                                });
+                    } else {
+                        testContext.failNow(ar1.cause());
+                    }
+                });
+        testContext.awaitCompletion(5, TimeUnit.SECONDS);
+    }
+
+    private OperationYaml parseYaml(String yaml) {
+        OperationYaml operationYaml = null;
+        try {
+            operationYaml = yamlMapper.readValue(yaml,
+                    OperationYaml.class);
+        } catch (JsonProcessingException e) {
+            fail(e);
+        }
+        return operationYaml;
+    }
 }

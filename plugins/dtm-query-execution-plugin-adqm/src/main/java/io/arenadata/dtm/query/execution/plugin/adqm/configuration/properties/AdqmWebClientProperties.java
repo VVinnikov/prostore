ab6d36b1093226938256e65e00d4d33b5f74f557
DMantasheva <mds@arenadata.io>
2021-01-22 10:52:38 +0300

    @ConfigurationProperties full

--- plugins/dtm-query-execution-plugin-adqm/src/main/java/io/arenadata/dtm/query/execution/plugin/adqm/configuration/properties/AdqmWebClientProperties.java
+++ plugins/dtm-query-execution-plugin-adqm/src/main/java/io/arenadata/dtm/query/execution/plugin/adqm/configuration/properties/AdqmWebClientProperties.java
@@ -3,7 +3,6 @@ package io.arenadata.dtm.query.execution.plugin.adqm.configuration.properties;
 import io.vertx.ext.web.client.WebClientOptions;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties("adqm.web-client")
 public class AdqmWebClientProperties extends WebClientOptions {
     private static final int DEFAULT_MAX_POOL_SIZE = 100;
 

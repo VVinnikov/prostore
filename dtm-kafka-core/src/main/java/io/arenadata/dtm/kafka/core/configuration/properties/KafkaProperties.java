0cdd1c8bb53c5cb2fc0e2c88d635049f98738d02
DMantasheva <mds@arenadata.io>
2021-01-23 22:54:02 +0300

    yml edit

--- dtm-kafka-core/src/main/java/io/arenadata/dtm/kafka/core/configuration/properties/KafkaProperties.java
+++ dtm-kafka-core/src/main/java/io/arenadata/dtm/kafka/core/configuration/properties/KafkaProperties.java
@@ -6,6 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component("coreKafkaProperties")
+@ConfigurationProperties(prefix = "core.kafka")
 @Data
 public class KafkaProperties {
     KafkaProducerProperty producer = new KafkaProducerProperty();

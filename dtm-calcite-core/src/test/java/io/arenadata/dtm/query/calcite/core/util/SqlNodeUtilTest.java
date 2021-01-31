24f0fa4f2aad4bc6219cd6ff2560fa55c89d5166
Ivan Lapa <lin@arenadata.io>
2021-01-31 17:58:17 +0300

    DTM-919 fix copy SqlNode

---
+++ dtm-calcite-core/src/test/java/io/arenadata/dtm/query/calcite/core/util/SqlNodeUtilTest.java
@@ -0,0 +1,72 @@
+package io.arenadata.dtm.query.calcite.core.util;
+
+import io.arenadata.dtm.query.calcite.core.configuration.CalciteCoreConfiguration;
+import io.arenadata.dtm.query.calcite.core.service.DefinitionService;
+import io.arenadata.dtm.query.calcite.core.service.impl.CalciteDefinitionService;
+import org.apache.calcite.avatica.util.Casing;
+import org.apache.calcite.avatica.util.Quoting;
+import org.apache.calcite.config.Lex;
+import org.apache.calcite.sql.SqlNode;
+import org.apache.calcite.sql.parser.SqlParser;
+import org.apache.calcite.sql.validate.SqlConformanceEnum;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SqlNodeUtilTest {
+    public static final DefinitionService<SqlNode> DEFINITION_SERVICE =
+            new CalciteDefinitionService(
+                    SqlParser.configBuilder()
+                            .setParserFactory(new CalciteCoreConfiguration().eddlParserImplFactory())
+                            .setConformance(SqlConformanceEnum.DEFAULT)
+                            .setLex(Lex.MYSQL)
+                            .setCaseSensitive(false)
+                            .setUnquotedCasing(Casing.TO_LOWER)
+                            .setQuotedCasing(Casing.TO_LOWER)
+                            .setQuoting(Quoting.DOUBLE_QUOTE)
+                            .build()) {
+            };
+
+    @Test
+    void copySimpleSelect() {
+        assertCopied("SELECT count(*) from dtm_918.accounts");
+    }
+
+    @Test
+    void copyUncommitDelta() {
+        assertCopied(
+                "SELECT count(*) from dtm_918.accounts FOR SYSTEM_TIME AS OF LATEST_UNCOMMITTED_DELTA"
+        );
+    }
+
+    @Test
+    void copySimpleSelectWhere() {
+        assertCopied("SELECT count(*) from dtm_918.accounts where id = 1");
+    }
+
+    @Test
+    void copySimpleSelectJoinWhere() {
+        assertCopied("SELECT t1.id, t1.name as name, 10 as code from dtm_918.accounts t1" +
+                " join transactions t2 on t2.id = t1.id where id = 1");
+    }
+
+    @Test
+    void copySimpleSelectJoinWhereLimit() {
+        assertCopied("SELECT t1.id, t1.name as name, 10 as code from dtm_918.accounts t1" +
+                " join transactions FOR SYSTEM_TIME AS OF LATEST_UNCOMMITTED_DELTA t2 on t2.id = t1.id where id = 1 limit 10");
+    }
+
+    @Test
+    void copySimpleSelectJoinWhereOrderBy() {
+        assertCopied("SELECT t1.id, t1.name as name, 10 as code from dtm_918.accounts t1" +
+                " join transactions FOR SYSTEM_TIME AS OF LATEST_UNCOMMITTED_DELTA t2 on t2.id = t1.id where id = 1" +
+                " order by t1.id");
+    }
+
+    private void assertCopied(String sql) {
+        SqlNode expected = DEFINITION_SERVICE.processingQuery(sql);
+        SqlNode actual = SqlNodeUtil.copy(expected);
+        System.out.println(actual.toString());
+        assertEquals(expected.toString(), actual.toString());
+    }
+}

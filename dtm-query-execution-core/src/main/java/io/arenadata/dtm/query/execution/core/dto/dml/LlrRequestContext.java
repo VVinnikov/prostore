06177e600c6dc80bb7ce431c0e10388d65c5e024
Ivan Lapa <lin@arenadata.io>
2021-01-31 18:00:02 +0300

    DTM-919 fix execute LLR request in Information schema and plugins

--- dtm-query-execution-core/src/main/java/io/arenadata/dtm/query/execution/core/dto/dml/LlrRequestContext.java
+++ dtm-query-execution-core/src/main/java/io/arenadata/dtm/query/execution/core/dto/dml/LlrRequestContext.java
@@ -4,6 +4,7 @@ import io.arenadata.dtm.common.delta.DeltaInformation;
 import io.arenadata.dtm.common.reader.QuerySourceRequest;
 import lombok.Builder;
 import lombok.Data;
+import org.apache.calcite.sql.SqlNode;
 
 import java.util.List;
 
@@ -12,13 +13,16 @@ public class LlrRequestContext {
     private List<DeltaInformation> deltaInformations;
     private DmlRequestContext dmlRequestContext;
     private QuerySourceRequest sourceRequest;
+    private SqlNode originalQuery;
 
     @Builder
     public LlrRequestContext(List<DeltaInformation> deltaInformations,
                              DmlRequestContext dmlRequestContext,
-                             QuerySourceRequest sourceRequest) {
+                             QuerySourceRequest sourceRequest,
+                             SqlNode originalQuery) {
         this.deltaInformations = deltaInformations;
         this.dmlRequestContext = dmlRequestContext;
         this.sourceRequest = sourceRequest;
+        this.originalQuery = originalQuery;
     }
 }

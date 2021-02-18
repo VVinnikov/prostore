320c5e10ec4ae5fa82a752e049b3e38c716d4842
Ivan Lapa <lin@arenadata.io>
2021-02-18 11:04:27 +0300

    DTM-965 Adb plugin: moved data transfer from staging to actual data in a separate request

--- plugins/dtm-query-execution-plugin-adb/src/main/java/io/arenadata/dtm/query/execution/plugin/adb/factory/MppwRequestFactory.java
+++ plugins/dtm-query-execution-plugin-adb/src/main/java/io/arenadata/dtm/query/execution/plugin/adb/factory/MppwRequestFactory.java
@@ -1,11 +1,8 @@
 package io.arenadata.dtm.query.execution.plugin.adb.factory;
 
-import io.arenadata.dtm.common.plugin.sql.PreparedStatementRequest;
 import io.arenadata.dtm.query.execution.plugin.adb.service.impl.mppw.dto.MppwTransferDataRequest;
 
-import java.util.List;
+public interface MppwRequestFactory<T> {
 
-public interface MppwRequestFactory {
-
-	List<PreparedStatementRequest> create(MppwTransferDataRequest request);
+    T create(MppwTransferDataRequest request);
 }

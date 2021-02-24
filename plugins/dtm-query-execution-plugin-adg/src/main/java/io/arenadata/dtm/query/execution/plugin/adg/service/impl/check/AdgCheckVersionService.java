963e2c0be36b69c2b52b537b0ef2f227d249377c
Ivan Lapa <lin@arenadata.io>
2021-02-24 08:30:36 +0300

    DTM-967 ADG: integrate CheckVersionService

---
+++ plugins/dtm-query-execution-plugin-adg/src/main/java/io/arenadata/dtm/query/execution/plugin/adg/service/impl/check/AdgCheckVersionService.java
@@ -0,0 +1,17 @@
+package io.arenadata.dtm.query.execution.plugin.adg.service.impl.check;
+
+import io.arenadata.dtm.common.version.VersionInfo;
+import io.arenadata.dtm.query.execution.plugin.api.check.CheckVersionRequest;
+import io.arenadata.dtm.query.execution.plugin.api.service.check.CheckVersionService;
+import io.vertx.core.Future;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("adgCheckVersionService")
+public class AdgCheckVersionService implements CheckVersionService {
+    @Override
+    public Future<List<VersionInfo>> checkVersion(CheckVersionRequest request) {
+        return null;
+    }
+}

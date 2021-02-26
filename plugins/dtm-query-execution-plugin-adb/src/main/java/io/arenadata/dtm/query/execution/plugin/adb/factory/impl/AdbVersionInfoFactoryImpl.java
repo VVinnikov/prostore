3a457a81095729b650a727979bde7ecccd56643e
ViktorArkhipov <vitek.arkhipov@gmail.com>
2021-02-26 18:41:27 +0500

    DTM-974 implemented adb version service

---
+++ plugins/dtm-query-execution-plugin-adb/src/main/java/io/arenadata/dtm/query/execution/plugin/adb/factory/impl/AdbVersionInfoFactoryImpl.java
@@ -0,0 +1,24 @@
+package io.arenadata.dtm.query.execution.plugin.adb.factory.impl;
+
+import io.arenadata.dtm.common.version.VersionInfo;
+import io.arenadata.dtm.query.execution.plugin.adb.factory.AdbVersionInfoFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static io.arenadata.dtm.query.execution.plugin.adb.factory.impl.AdbVersionQueriesFactoryImpl.COMPONENT_NAME_COLUMN;
+import static io.arenadata.dtm.query.execution.plugin.adb.factory.impl.AdbVersionQueriesFactoryImpl.VERSION_COLUMN;
+
+@Service
+public class AdbVersionInfoFactoryImpl implements AdbVersionInfoFactory {
+
+    @Override
+    public List<VersionInfo> create(List<Map<String, Object>> rows) {
+        return rows.stream()
+                .map(row -> new VersionInfo(row.get(COMPONENT_NAME_COLUMN).toString(),
+                        row.get(VERSION_COLUMN).toString()))
+                .collect(Collectors.toList());
+    }
+}

e9484f6e4136bfa5d1b1fcbd6d5a8a9be9cf3f40
Valery <vbir94@gmail.com>
2021-02-26 17:33:53 +0400

    DTM-989 Fix null value display

--- dtm-jdbc-driver/src/main/java/io/arenadata/dtm/jdbc/ext/DtmResultSet.java
+++ dtm-jdbc-driver/src/main/java/io/arenadata/dtm/jdbc/ext/DtmResultSet.java
@@ -113,6 +113,9 @@ public class DtmResultSet implements ResultSet {
     @Override
     public Object getObject(int columnIndex) throws SQLException {
         final ColumnMetadata columnMetadata = this.metadata.get(columnIndex - 1);
+        if (this.getValue(columnIndex) == null) {
+            return null;
+        }
         switch (columnMetadata.getType()) {
             case INT:
             case BIGINT:

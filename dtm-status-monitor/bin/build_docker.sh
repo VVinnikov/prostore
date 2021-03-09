b6ff0886a37f77782a62d5f2f3a6c32548e99d44
Ivan Lapa <lin@arenadata.io>
2021-03-09 19:46:32 +0300

    bump version to 3.5.0

---
+++ dtm-status-monitor/bin/build_docker.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+DTM_JAR_NAME=$(find target -iname 'dtm-status-monitor-*.jar' | head -n 1)
+if [ -z "${DTM_JAR_NAME}" ]; then
+  echo 'Cannot find DTM JAR. Exiting'
+  exit 1
+fi
+
+docker build --build-arg DTM_JAR="${DTM_JAR_NAME}" -t ci.arenadata.io/dtm-status-monitor:latest .

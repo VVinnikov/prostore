2adc3240275421e8c6f5c818b6be4ac34edd2a17
Ivan Lapa <lin@arenadata.io>
2021-03-12 11:08:38 +0300

    DTM-1029 fix comments

--- plugins/dtm-query-execution-plugin-adqm/src/main/java/io/arenadata/dtm/query/execution/plugin/adqm/service/impl/AdqmTemplateParameterConverter.java
+++ plugins/dtm-query-execution-plugin-adqm/src/main/java/io/arenadata/dtm/query/execution/plugin/adqm/service/impl/AdqmTemplateParameterConverter.java
@@ -3,6 +3,7 @@ package io.arenadata.dtm.query.execution.plugin.adqm.service.impl;
 import io.arenadata.dtm.query.execution.plugin.adqm.service.TemplateParameterConverter;
 import org.apache.calcite.sql.SqlLiteral;
 import org.apache.calcite.sql.SqlNode;
+import org.apache.calcite.sql.type.SqlTypeName;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,7 +19,7 @@ public class AdqmTemplateParameterConverter implements TemplateParameterConverte
     }
 
     protected SqlNode convertParam(SqlNode param) {
-        if (param instanceof SqlLiteral && ((SqlLiteral) param).getTypeName().getName().equals("BOOLEAN")) {
+        if (param instanceof SqlLiteral && SqlTypeName.BOOLEAN.equals(((SqlLiteral) param).getTypeName())) {
             Boolean aBoolean = ((SqlLiteral) param).getValueAs(Boolean.class);
             if (aBoolean == null) {
                 return SqlLiteral.createNull(param.getParserPosition());

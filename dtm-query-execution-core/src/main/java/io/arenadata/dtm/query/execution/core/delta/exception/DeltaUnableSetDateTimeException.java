bc64f95d57844017336650b657cda51e96646b5e
ViktorArkhipov <vitek.arkhipov@gmail.com>
2021-04-29 17:35:30 +0500

    DTM-1055 changed DeltaUnableSetDateTimeException message

--- dtm-query-execution-core/src/main/java/io/arenadata/dtm/query/execution/core/delta/exception/DeltaUnableSetDateTimeException.java
+++ dtm-query-execution-core/src/main/java/io/arenadata/dtm/query/execution/core/delta/exception/DeltaUnableSetDateTimeException.java
@@ -2,6 +2,6 @@ package io.arenadata.dtm.query.execution.core.delta.exception;
 
 public class DeltaUnableSetDateTimeException extends DeltaException {
     public DeltaUnableSetDateTimeException(String dateTime, String actualDateTime) {
-        super(String.format("Unable to set the date-time %s preceding the actual delta %s", dateTime, actualDateTime));
+        super(String.format("Unable to set the date-time %s preceding or equal the actual delta %s", dateTime, actualDateTime));
     }
 }

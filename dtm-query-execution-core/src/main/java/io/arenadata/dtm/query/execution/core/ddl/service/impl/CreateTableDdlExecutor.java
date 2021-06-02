84ee67b806098c82316cbb7e6c397bd1587c3a1c
Valery <vbir94@gmail.com>
2021-06-02 14:18:40 +0400

    DTM-1162 Conflict resolve

--- dtm-query-execution-core/src/main/java/io/arenadata/dtm/query/execution/core/ddl/service/impl/CreateTableDdlExecutor.java
+++ dtm-query-execution-core/src/main/java/io/arenadata/dtm/query/execution/core/ddl/service/impl/CreateTableDdlExecutor.java
@@ -7,9 +7,7 @@ import io.arenadata.dtm.common.reader.QueryResult;
 import io.arenadata.dtm.common.reader.SourceType;
 import io.arenadata.dtm.query.calcite.core.extension.ddl.SqlCreateTable;
 import io.arenadata.dtm.query.execution.core.base.exception.datamart.DatamartNotExistsException;
-import io.arenadata.dtm.query.execution.core.base.exception.datamart.DatamartNotExistsException;
 import io.arenadata.dtm.query.execution.core.base.exception.entity.EntityAlreadyExistsException;
-import io.arenadata.dtm.query.execution.core.base.exception.table.ValidationDtmException;
 import io.arenadata.dtm.query.execution.core.base.repository.ServiceDbFacade;
 import io.arenadata.dtm.query.execution.core.base.repository.zookeeper.DatamartDao;
 import io.arenadata.dtm.query.execution.core.base.repository.zookeeper.EntityDao;
@@ -94,40 +92,6 @@ public class CreateTableDdlExecutor extends QueryResultDdlExecutor {
     private void validateFields(List<EntityField> fields) {
         checkRequiredKeys(fields);
         checkVarcharSize(fields);
-    }
-
-    private void checkVarcharSize(List<EntityField> fields) {
-        List<String> notSetSizeFields = fields.stream()
-                .filter(field -> field.getType() == ColumnType.CHAR)
-                .filter(field -> field.getSize() == null)
-                .map(EntityField::getName)
-                .collect(Collectors.toList());
-        if (!notSetSizeFields.isEmpty()) {
-            throw new ValidationDtmException(
-                    String.format("Specifying the size for columns%s with types[VARCHAR, CHAR] is required", notSetSizeFields)
-            );
-        }
-    }
-
-    private void checkRequiredKeys(List<EntityField> fields) {
-        val notExistsKeys = new ArrayList<String>();
-        val notExistsPrimaryKeys = fields.stream()
-                .noneMatch(f -> f.getPrimaryOrder() != null);
-        if (notExistsPrimaryKeys) {
-            notExistsKeys.add("primary key(s)");
-        }
-
-        val notExistsShardingKey = fields.stream()
-                .noneMatch(f -> f.getShardingOrder() != null);
-        if (notExistsShardingKey) {
-            notExistsKeys.add("sharding key(s)");
-        }
-
-        if (!notExistsKeys.isEmpty()) {
-            throw new ValidationDtmException(
-                    String.format("Primary keys and Sharding keys are required. The following keys do not exist: %s",
-                            String.join(",", notExistsKeys)));
-        }
     }
 
     private Future<Void> getEntityAlreadyExistsFuture(String entityNameWithSchema) {

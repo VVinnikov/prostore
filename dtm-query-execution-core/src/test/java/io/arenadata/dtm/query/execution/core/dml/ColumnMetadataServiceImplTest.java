51f45b3c8ae589635564b4d4c0f77fc96186da34
Valery <vbir94@gmail.com>
2021-06-04 11:02:55 +0400

    DTM-1160 Fix unit tests

--- dtm-query-execution-core/src/test/java/io/arenadata/dtm/query/execution/core/dml/ColumnMetadataServiceImplTest.java
+++ dtm-query-execution-core/src/test/java/io/arenadata/dtm/query/execution/core/dml/ColumnMetadataServiceImplTest.java
@@ -4,15 +4,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import io.arenadata.dtm.common.dto.QueryParserRequest;
 import io.arenadata.dtm.common.model.ddl.ColumnType;
-import io.arenadata.dtm.common.reader.QueryRequest;
-import io.arenadata.dtm.common.reader.QuerySourceRequest;
 import io.arenadata.dtm.query.calcite.core.service.QueryParserService;
+import io.arenadata.dtm.query.execution.core.calcite.configuration.CalciteConfiguration;
+import io.arenadata.dtm.query.execution.core.calcite.factory.CoreCalciteSchemaFactory;
+import io.arenadata.dtm.query.execution.core.calcite.factory.CoreSchemaFactory;
 import io.arenadata.dtm.query.execution.core.calcite.service.CoreCalciteContextProvider;
 import io.arenadata.dtm.query.execution.core.calcite.service.CoreCalciteDMLQueryParserService;
-import io.arenadata.dtm.query.execution.core.calcite.factory.CoreCalciteSchemaFactory;
-import io.arenadata.dtm.query.execution.core.calcite.configuration.CalciteConfiguration;
 import io.arenadata.dtm.query.execution.core.dml.service.ColumnMetadataService;
-import io.arenadata.dtm.query.execution.core.calcite.factory.CoreSchemaFactory;
 import io.arenadata.dtm.query.execution.core.dml.service.impl.ColumnMetadataServiceImpl;
 import io.arenadata.dtm.query.execution.core.utils.TestUtils;
 import io.arenadata.dtm.query.execution.model.metadata.ColumnMetadata;
@@ -49,20 +47,27 @@ class ColumnMetadataServiceImplTest {
     @Test
     void getColumnMetadata() throws JsonProcessingException, InterruptedException {
         val testContext = new VertxTestContext();
-        QuerySourceRequest sourceRequest = new QuerySourceRequest();
-        QueryRequest queryRequest = new QueryRequest();
-        queryRequest.setSql("select * from dml.accounts");
-        sourceRequest.setQueryRequest(queryRequest);
+        val sql = "select * from dml.accounts";
         val datamarts = DatabindCodec.mapper()
-                .readValue(loadTextFromFile("schema/dml.json"), new TypeReference<List<Datamart>>() {
+                .readValue(loadTextFromFile("schema/dml_all_types.json"), new TypeReference<List<Datamart>>() {
                 });
-        sourceRequest.setLogicalSchema(datamarts);
         List<ColumnMetadata> expectedColumns = Arrays.asList(
-                new ColumnMetadata("account_id", ColumnType.BIGINT),
-                new ColumnMetadata("account_type", ColumnType.VARCHAR, 1));
-        SqlNode sqlNode = TestUtils.DEFINITION_SERVICE.processingQuery(queryRequest.getSql());
-        service.getColumnMetadata(new QueryParserRequest(sqlNode,
-                sourceRequest.getLogicalSchema()))
+                new ColumnMetadata("id", ColumnType.INT),
+                new ColumnMetadata("double_col", ColumnType.DOUBLE),
+                new ColumnMetadata("float_col", ColumnType.FLOAT),
+                new ColumnMetadata("varchar_col", ColumnType.VARCHAR, 36),
+                new ColumnMetadata("boolean_col", ColumnType.BOOLEAN),
+                new ColumnMetadata("int_col", ColumnType.INT),
+                new ColumnMetadata("bigint_col", ColumnType.BIGINT),
+                new ColumnMetadata("date_col", ColumnType.DATE),
+                new ColumnMetadata("timestamp_col", ColumnType.TIMESTAMP, 6),
+                new ColumnMetadata("time_col", ColumnType.TIME, 5),
+                new ColumnMetadata("uuid_col", ColumnType.VARCHAR, 36),
+                new ColumnMetadata("char_col", ColumnType.CHAR, 10),
+                new ColumnMetadata("int32_col", ColumnType.INT32),
+                new ColumnMetadata("link_col", ColumnType.LINK));
+        SqlNode sqlNode = TestUtils.DEFINITION_SERVICE.processingQuery(sql);
+        service.getColumnMetadata(new QueryParserRequest(sqlNode, datamarts))
                 .onComplete(ar -> {
                     if (ar.succeeded()) {
                         log.info("Result columns: {}", ar.result());

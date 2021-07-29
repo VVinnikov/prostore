3bf0e466fec4ec2043cedf0fe4545546877f33cb
Vyacheslav Puzkov <pva@arenadata.io>
2021-07-29 13:03:04 +0700

    DTM-1271 update to 5.0.0 snapshot

--- dtm-jdbc-driver/src/main/java/io/arenadata/dtm/jdbc/util/DriverInfo.java
+++ dtm-jdbc-driver/src/main/java/io/arenadata/dtm/jdbc/util/DriverInfo.java
@@ -4,8 +4,8 @@ public class DriverInfo {
     public static final String DATABASE_PRODUCT_NAME = "DTM";
     public static final String DRIVER_NAME = "DTM JDBC Driver";
     public static final String DRIVER_SHORT_NAME = "DtmJDBC";
-    public static final int MAJOR_VERSION = 4;
-    public static final int MINOR_VERSION = 1;
+    public static final int MAJOR_VERSION = 5;
+    public static final int MINOR_VERSION = 0;
     public static final int PATCH_VERSION = 0;
     public static final String DRIVER_VERSION = String.format("%s.%s.%s", MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION);
     public static final String DRIVER_FULL_NAME = DRIVER_NAME + " " + DRIVER_VERSION;
